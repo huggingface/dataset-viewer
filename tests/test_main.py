20e10053a5755c514915dc3d68b7179a429e9439
Sylvain Lesage <severo@rednegra.net>
2021-07-28 10:27:25 +0200

    test: 💍 add test on unknown split
    also: remove unused variable

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from datasets import list_datasets
 
 from datasets_preview_backend.main import get_dataset_extract
 
@@ -15,11 +16,17 @@ def test_extract_num_rows():
     assert extract[0]["tokens"][0] == "What"
 
 
+def test_extract_unknown_split():
+    # "aeslc" dataset has no "train" split, while "train" is the hardcoded split used to download
+    extract = get_dataset_extract("aeslc", 100)
+    assert len(extract) == 0
+
+
 def test_extract_unknown_model():
     with pytest.raises(FileNotFoundError):
-        extract = get_dataset_extract("doesnotexist", 100)
+        get_dataset_extract("doesnotexist", 100)
 
 
 def test_extract_subset_not_implemented():
     with pytest.raises(ValueError, match="Config name is missing..*"):
-        extract = get_dataset_extract("glue", 100)
+        get_dataset_extract("glue", 100)
