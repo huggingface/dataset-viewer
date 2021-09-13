bab89546effc029a8913206e428b98b1ae39ce0b
Sylvain Lesage <sylvain.lesage@huggingface.co>
2021-09-13 19:19:49 +0000

    feat: 🎸 upgrade to datasets 1.12.0
    also: prepare a test (but let it disabled for now due to an issue in
    datasets)

--- tests/queries/test_rows.py
+++ tests/queries/test_rows.py
@@ -131,3 +131,10 @@ def test_txt_zip():
     extract = extract_rows(dataset, config, split, num_rows)
     rows = extract["rows"]
     assert len(rows) == 20
+
+
+# UNCOMMENT THIS WHEN https://github.com/huggingface/datasets/issues/2901 is fixed
+# def test_pathlib():
+#     # see https://github.com/huggingface/datasets/issues/2866
+#     extract = extract_rows("counter", DEFAULT_CONFIG_NAME, "train", 10)
+#     assert len(extract["rows"]) == 10
