b12c65bf11c49e2e2c372adee66e5fb4f71dd1e2
Sylvain Lesage <sylvain.lesage@huggingface.co>
2021-09-17 09:19:30 +0000

    refactor: 💡 factorize the responses

--- src/datasets_preview_backend/routes.py
+++ src/datasets_preview_backend/routes.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Union
+from typing import Optional, Union
 
 from starlette.requests import Request
 from starlette.responses import JSONResponse, PlainTextResponse
@@ -24,6 +24,35 @@ def log_error(err: StatusError):
     logger.debug(f"Caused by a {type(err.__cause__).__name__}: '{str(err.__cause__)}'")
 
 
+def get_response(
+    route: str,
+    dataset: str,
+    config: Optional[str] = None,
+    split: Optional[str] = None,
+    num_rows: Optional[int] = None,
+    use_auth_token: Optional[Union[str, None]] = None,
+):
+    try:
+        if route == "/info":
+            logger.info(f"/info, dataset={dataset}")
+            content = get_info(dataset, use_auth_token)
+        elif route == "/configs":
+            logger.info(f"/configs, dataset={dataset}")
+            content = get_configs(dataset, use_auth_token)
+        elif route == "/splits":
+            logger.info(f"/splits, dataset={dataset}, config={config}")
+            content = get_splits(dataset, config, use_auth_token)
+        elif route == "/rows":
+            logger.info(f"/rows, dataset={dataset}, config={config}, split={split}, num_rows={num_rows}")
+            content = extract_rows(dataset, config, split, num_rows, use_auth_token)
+        else:
+            raise Exception(f"unknown route {route}")
+        return {"content": content, "status_code": 200}
+    except (Status400Error, Status404Error) as err:
+        log_error(err)
+        return {"content": err.as_dict(), "status_code": err.status_code}
+
+
 async def healthcheck(_: Request):
     logger.info("/healthcheck")
     return PlainTextResponse("ok")
@@ -33,26 +62,16 @@ async def info(request: Request):
     dataset: str = request.query_params.get("dataset")
     use_auth_token: Union[str, None] = get_token(request)
 
-    try:
-        logger.info(f"/info, dataset={dataset}")
-        return JSONResponse(get_info(dataset, use_auth_token=use_auth_token))
-    except (Status400Error, Status404Error) as err:
-        log_error(err)
-        return JSONResponse(err.as_dict(), status_code=err.status_code)
-    # other exceptions will generate a 500 response
+    response = get_response("/info", dataset=dataset, use_auth_token=use_auth_token)
+    return JSONResponse(response["content"], status_code=response["status_code"])
 
 
 async def configs(request: Request):
     dataset: str = request.query_params.get("dataset")
     use_auth_token: Union[str, None] = get_token(request)
 
-    try:
-        logger.info(f"/configs, dataset={dataset}")
-        return JSONResponse(get_configs(dataset, use_auth_token=use_auth_token))
-    except (Status400Error, Status404Error) as err:
-        log_error(err)
-        return JSONResponse(err.as_dict(), status_code=err.status_code)
-    # other exceptions will generate a 500 response
+    response = get_response("/configs", dataset=dataset, use_auth_token=use_auth_token)
+    return JSONResponse(response["content"], status_code=response["status_code"])
 
 
 async def splits(request: Request):
@@ -60,13 +79,8 @@ async def splits(request: Request):
     config: Union[str, None] = request.query_params.get("config")
     use_auth_token: Union[str, None] = get_token(request)
 
-    try:
-        logger.info(f"/splits, dataset={dataset}, config={config}")
-        return JSONResponse(get_splits(dataset, config, use_auth_token=use_auth_token))
-    except (Status400Error, Status404Error) as err:
-        log_error(err)
-        return JSONResponse(err.as_dict(), status_code=err.status_code)
-    # other exceptions will generate a 500 response
+    response = get_response("/splits", dataset=dataset, config=config, use_auth_token=use_auth_token)
+    return JSONResponse(response["content"], status_code=response["status_code"])
 
 
 async def rows(request: Request):
@@ -76,10 +90,7 @@ async def rows(request: Request):
     num_rows = get_int_value(d=request.query_params, key="rows", default=EXTRACT_ROWS_LIMIT)
     use_auth_token: Union[str, None] = get_token(request)
 
-    try:
-        logger.info(f"/rows, dataset={dataset}, config={config}, split={split}, num_rows={num_rows}")
-        return JSONResponse(extract_rows(dataset, config, split, num_rows, use_auth_token=use_auth_token))
-    except (Status400Error, Status404Error) as err:
-        log_error(err)
-        return JSONResponse(err.as_dict(), status_code=err.status_code)
-    # other exceptions will generate a 500 response
+    response = get_response(
+        "/rows", dataset=dataset, config=config, split=split, num_rows=num_rows, use_auth_token=use_auth_token
+    )
+    return JSONResponse(response["content"], status_code=response["status_code"])
