8276b3a7c382b1da85e2451d5e0c46185f02621b
Sylvain Lesage <sylvain.lesage@huggingface.co>
2021-09-23 08:24:48 +0000

    feat: 🎸 add env var to limit the cache size

--- README.md
+++ README.md
@@ -30,7 +30,8 @@ Set environment variables to configure the following aspects:
 
 - `APP_HOSTNAME`: the hostname used by the app. Defaults to `"localhost"`.
 - `APP_PORT`: the port used by the app. Defaults to `8000`.
-- `CACHE_TTL_SECONDS`: number of seconds the requests are kept in cached. Defaults to `604800`.
+- `CACHE_SIZE_LIMIT`: maximum size of the cache in bytes. Defaults to `1073741824` (1 GiB).
+- `CACHE_TTL_SECONDS`: number of seconds the entries are kept in cached. Defaults to `604800`.
 - `DATASETS_ENABLE_PRIVATE`: enable private datasets. Defaults to `False`.
 - `EXTRACT_ROWS_LIMIT`: number of rows in the extract, if not specified in the API request. Defaults to `100`.
 - `HF_TOKEN`: the token to be able to download the private datasets from the hub (see https://huggingface.co/settings/token). Defaults to empty.
--- src/datasets_preview_backend/config.py
+++ src/datasets_preview_backend/config.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from datasets_preview_backend.constants import (
     DEFAULT_APP_HOSTNAME,
     DEFAULT_APP_PORT,
+    DEFAULT_CACHE_SIZE_LIMIT,
     DEFAULT_CACHE_TTL_SECONDS,
     DEFAULT_DATASETS_ENABLE_PRIVATE,
     DEFAULT_EXTRACT_ROWS_LIMIT,
@@ -25,6 +26,7 @@ load_dotenv()
 
 APP_HOSTNAME = os.environ.get("APP_HOSTNAME", DEFAULT_APP_HOSTNAME)
 APP_PORT = get_int_value(d=os.environ, key="APP_PORT", default=DEFAULT_APP_PORT)
+CACHE_SIZE_LIMIT = get_int_value(d=os.environ, key="CACHE_SIZE_LIMIT", default=DEFAULT_CACHE_SIZE_LIMIT)
 CACHE_TTL_SECONDS = get_int_value(d=os.environ, key="CACHE_TTL_SECONDS", default=DEFAULT_CACHE_TTL_SECONDS)
 DATASETS_ENABLE_PRIVATE = get_bool_value(
     d=os.environ, key="DATASETS_ENABLE_PRIVATE", default=DEFAULT_DATASETS_ENABLE_PRIVATE
@@ -33,4 +35,4 @@ EXTRACT_ROWS_LIMIT = get_int_value(d=os.environ, key="EXTRACT_ROWS_LIMIT", defau
 HF_TOKEN = get_str_or_none_value(d=os.environ, key="HF_TOKEN", default=DEFAULT_HF_TOKEN)
 LOG_LEVEL = get_str_value(d=os.environ, key="LOG_LEVEL", default=DEFAULT_LOG_LEVEL)
 
-cache = Cache()
+cache = Cache(size_limit=CACHE_SIZE_LIMIT)
