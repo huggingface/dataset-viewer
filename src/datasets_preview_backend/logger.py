8a011fe70e01ab82f9a127e124a795d00e0d2869
Sylvain Lesage <sylvain.lesage@huggingface.co>
2021-10-05 12:52:54 +0000

    feat: 🎸 set the datasets library logs at the lowest level

--- src/datasets_preview_backend/logger.py
+++ src/datasets_preview_backend/logger.py
@@ -1,5 +1,7 @@
 import logging
 
+import datasets
+
 from datasets_preview_backend.constants import DEFAULT_LOG_LEVEL
 
 
@@ -13,5 +15,8 @@ def init_logger(log_level: str = DEFAULT_LOG_LEVEL) -> None:
     logHandler = logging.StreamHandler()
     logHandler.setFormatter(formatter)
     logger.addHandler(logHandler)
+
+    # set logs from the datasets library to the least verbose
+    datasets.utils.logging.set_verbosity(datasets.utils.logging.log_levels["critical"])
 
     logger.debug(f"Log level set to: {logging.getLevelName(logger.getEffectiveLevel())}")
