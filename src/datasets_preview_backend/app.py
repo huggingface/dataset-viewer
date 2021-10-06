984c5555fe78f913e35484672b5d43cee13686be
Sylvain Lesage <sylvain.lesage@huggingface.co>
2021-10-06 08:03:08 +0000

    feat: 🎸 enable concurrency
    With a persistent cache, all the uvicorn workers can use the same cache

--- src/datasets_preview_backend/app.py
+++ src/datasets_preview_backend/app.py
@@ -2,7 +2,7 @@ import uvicorn  # type: ignore
 from starlette.applications import Starlette
 from starlette.routing import Route
 
-from datasets_preview_backend.config import APP_HOSTNAME, APP_PORT
+from datasets_preview_backend.config import APP_HOSTNAME, APP_PORT, CACHE_PERSIST, WEB_CONCURRENCY
 from datasets_preview_backend.routes import (
     CacheStats,
     Configs,
@@ -31,6 +31,7 @@ def create_app() -> Starlette:
 
 
 def start() -> None:
-    # the cache is not shared between workers for now, thus only one worker is allowed
-    WEB_CONCURRENCY = 1
-    uvicorn.run("app:create_app", host=APP_HOSTNAME, port=APP_PORT, factory=True, workers=WEB_CONCURRENCY)
+    # the cache is shared between workers only if CACHE_PERSIST is set to true
+    # if not, only one worker is allowed
+    web_concurrency = WEB_CONCURRENCY if CACHE_PERSIST else 1
+    uvicorn.run("app:create_app", host=APP_HOSTNAME, port=APP_PORT, factory=True, workers=web_concurrency)
