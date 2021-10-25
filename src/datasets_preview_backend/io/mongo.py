8f42e36c24d342dab0afd548a601329925a92be9
Sylvain Lesage <sylvain.lesage@huggingface.co>
2021-10-25 14:54:27 +0000

    feat: 🎸 add cache database name
    also: use mongoengine instead of pymongo

--- src/datasets_preview_backend/io/mongo.py
+++ src/datasets_preview_backend/io/mongo.py
@@ -1,19 +1,7 @@
-from pymongo.collection import Collection
-from pymongo.database import Database
-from pymongo.mongo_client import MongoClient
+from mongoengine import connect
 
 from datasets_preview_backend.config import MONGO_CACHE_DATABASE, MONGO_URL
 
 
-def get_client() -> MongoClient:
-    return MongoClient(MONGO_URL)
-
-
-def get_database() -> Database:
-    client = get_client()
-    return client[MONGO_CACHE_DATABASE]
-
-
-def get_datasets_collection() -> Collection:
-    database = get_database()
-    return database["datasets"]
+def connect_cache() -> None:
+    connect(MONGO_CACHE_DATABASE, host=MONGO_URL)
