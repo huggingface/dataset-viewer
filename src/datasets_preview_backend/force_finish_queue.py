dc41ca69b49e5e554f1d30f7d92f40c35c6162c2
Sylvain Lesage <sylvain.lesage@huggingface.co>
2021-10-27 08:54:56 +0000

    style: 💄 fix quality test

--- src/datasets_preview_backend/force_finish_queue.py
+++ src/datasets_preview_backend/force_finish_queue.py
@@ -1,7 +1,10 @@
 import logging
 
 from datasets_preview_backend.io.logger import init_logger
-from datasets_preview_backend.io.queue import connect_to_queue, force_finish_started_jobs
+from datasets_preview_backend.io.queue import (
+    connect_to_queue,
+    force_finish_started_jobs,
+)
 
 if __name__ == "__main__":
     init_logger("INFO", "force_finish_queue")
