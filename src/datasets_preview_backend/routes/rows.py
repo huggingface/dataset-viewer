dc6f9c408d018205f0009e68ec9419cb9eaaee9e
Test User <sylvain.lesage@huggingface.co>
2022-04-04 15:57:56 +0000

    feat: 🎸 give reason in error if dataset/split cache is refreshi
    fixes https://github.com/huggingface/datasets-preview-backend/issues/186

--- src/datasets_preview_backend/routes/rows.py
+++ src/datasets_preview_backend/routes/rows.py
@@ -8,8 +8,9 @@ from datasets_preview_backend.config import (
     ROWS_MAX_BYTES,
     ROWS_MIN_NUMBER,
 )
-from datasets_preview_backend.exceptions import StatusError
+from datasets_preview_backend.exceptions import Status400Error, StatusError
 from datasets_preview_backend.io.cache import get_rows_response
+from datasets_preview_backend.io.queue import is_split_in_queue
 from datasets_preview_backend.routes._utils import get_response
 
 logger = logging.getLogger(__name__)
@@ -22,11 +23,23 @@ async def rows_endpoint(request: Request) -> Response:
     logger.info(f"/rows, dataset={dataset_name}, config={config_name}, split={split_name}")
 
     try:
-        if not isinstance(dataset_name, str) or not isinstance(config_name, str) or not isinstance(split_name, str):
-            raise StatusError("Parameters 'dataset', 'config' and 'split' are required", 400)
-        rows_response, rows_error, status_code = get_rows_response(
-            dataset_name, config_name, split_name, ROWS_MAX_BYTES, ROWS_MIN_NUMBER
-        )
-        return get_response(rows_response or rows_error, status_code, MAX_AGE_LONG_SECONDS)
+        try:
+            if (
+                not isinstance(dataset_name, str)
+                or not isinstance(config_name, str)
+                or not isinstance(split_name, str)
+            ):
+                raise StatusError("Parameters 'dataset', 'config' and 'split' are required", 400)
+            rows_response, rows_error, status_code = get_rows_response(
+                dataset_name, config_name, split_name, ROWS_MAX_BYTES, ROWS_MIN_NUMBER
+            )
+            return get_response(rows_response or rows_error, status_code, MAX_AGE_LONG_SECONDS)
+        except StatusError as err:
+            if err.message == "Not found. The split does not exist." and is_split_in_queue(
+                dataset_name, config_name, split_name
+            ):
+                raise Status400Error("The split is being processed. Retry later.", err) from err
+            else:
+                raise err
     except StatusError as err:
         return get_response(err.as_content(), err.status_code, MAX_AGE_LONG_SECONDS)
