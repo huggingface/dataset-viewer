14d61f761c7b9c6298383a70ec7894db55fa7b05
Test User <sylvain.lesage@huggingface.co>
2022-06-30 15:38:04 +0000

    feat: 🎸 implement a new cache (simple_cache)
    It is a lot simpler in the sense that we simply store HTTP responses,
    with a lot less logic than in cache.

---
+++ libs/libcache/src/libcache/simple_cache.py
@@ -0,0 +1,230 @@
+import enum
+import logging
+import types
+from datetime import datetime, timezone
+from typing import Dict, Generic, List, Optional, Tuple, Type, TypedDict, TypeVar
+
+from mongoengine import Document, connect
+from mongoengine.fields import (
+    BooleanField,
+    DateTimeField,
+    DictField,
+    EnumField,
+    StringField,
+)
+from mongoengine.queryset.queryset import QuerySet
+
+# START monkey patching ### hack ###
+# see https://github.com/sbdchd/mongo-types#install
+U = TypeVar("U", bound=Document)
+
+
+def no_op(self, x):  # type: ignore
+    return self
+
+
+QuerySet.__class_getitem__ = types.MethodType(no_op, QuerySet)
+
+
+logger = logging.getLogger(__name__)
+
+
+class QuerySetManager(Generic[U]):
+    def __get__(self, instance: object, cls: Type[U]) -> QuerySet[U]:
+        return QuerySet(cls, cls._get_collection())
+
+
+# END monkey patching ### hack ###
+
+
+def connect_to_cache(database, host) -> None:
+    connect(database, alias="cache", host=host)
+
+
+# subset of https://docs.python.org/3/library/http.html#http.HTTPStatus
+class HTTPStatus(enum.Enum):
+    OK = "200"
+    BAD_REQUEST = "400"
+    INTERNAL_SERVER_ERROR = "500"
+
+
+def get_datetime() -> datetime:
+    return datetime.now(timezone.utc)
+
+
+# cache of the /splits endpoint
+class SplitsResponse(Document):
+    dataset_name = StringField(required=True, unique=True)
+    http_status = EnumField(HTTPStatus, required=True)
+    response = DictField(required=True)  # can be an error or a valid content. Not important here.
+    details = DictField(required=False)  # can be a detailed error when we don't want to put it in the response.
+    stale = BooleanField(required=False, default=False)
+    updated_at = DateTimeField(default=get_datetime)
+
+    meta = {
+        "collection": "splitsResponse",
+        "db_alias": "cache",
+        "indexes": ["dataset_name", "http_status", "stale"],
+    }
+    objects = QuerySetManager["SplitsResponse"]()
+
+
+# cache of the /first-rows endpoint
+class FirstRowsResponse(Document):
+    dataset_name = StringField(required=True, unique_with=["config_name", "split_name"])
+    config_name = StringField(required=True)
+    split_name = StringField(required=True)
+    http_status = EnumField(HTTPStatus, required=True)
+    response = DictField(required=True)  # can be an error or a valid content. Not important here.
+    details = DictField(required=False)  # can be a detailed error when we don't want to put it in the response.
+    stale = BooleanField(required=False, default=False)
+    updated_at = DateTimeField(default=get_datetime)
+
+    meta = {
+        "collection": "firstRowsResponse",
+        "db_alias": "cache",
+        "indexes": [
+            ("dataset_name", "config_name", "split_name"),
+            ("dataset_name", "http_status"),
+            ("http_status", "dataset_name"),
+            # ^ this index (reversed) is used for the "distinct" command to get the names of the valid datasets
+        ],
+    }
+    objects = QuerySetManager["FirstRowsResponse"]()
+
+
+AnyResponse = TypeVar("AnyResponse", SplitsResponse, FirstRowsResponse)
+
+# TODO: add logger.debug for each operation?
+
+
+# /splits endpoint
+# Note: we let the exceptions throw (ie DocumentTooLarge): it's the responsibility of the caller to manage them
+def upsert_splits_response(
+    dataset_name: str, response: Dict, http_status: HTTPStatus, details: Optional[Dict] = None
+) -> None:
+    SplitsResponse.objects(dataset_name=dataset_name).upsert_one(
+        http_status=http_status,
+        response=response,
+        stale=False,
+        details=details,
+        updated_at=get_datetime(),
+    )
+
+
+def delete_splits_responses(dataset_name: str):
+    SplitsResponse.objects(dataset_name=dataset_name).delete()
+
+
+def mark_splits_responses_as_stale(dataset_name: str):
+    SplitsResponse.objects(dataset_name=dataset_name).update(stale=True, updated_at=get_datetime())
+
+
+# Note: we let the exceptions throw (ie DoesNotExist): it's the responsibility of the caller to manage them
+def get_splits_response(dataset_name: str) -> Tuple[Dict, HTTPStatus]:
+    split_response = SplitsResponse.objects(dataset_name=dataset_name).get()
+    return split_response.response, split_response.http_status
+
+
+# /first-rows endpoint
+# Note: we let the exceptions throw (ie DocumentTooLarge): it's the responsibility of the caller to manage them
+def upsert_first_rows_response(
+    dataset_name: str,
+    config_name: str,
+    split_name: str,
+    response: Dict,
+    http_status: HTTPStatus,
+    details: Optional[Dict] = None,
+) -> None:
+    FirstRowsResponse.objects(dataset_name=dataset_name, config_name=config_name, split_name=split_name).upsert_one(
+        http_status=http_status, response=response, stale=False, details=details, updated_at=get_datetime()
+    )
+
+
+def delete_first_rows_responses(
+    dataset_name: str, config_name: Optional[str] = None, split_name: Optional[str] = None
+):
+    objects = (
+        FirstRowsResponse.objects(dataset_name=dataset_name)
+        if config_name is None
+        else FirstRowsResponse.objects(dataset_name=dataset_name, config_name=config_name, split_name=split_name)
+    )
+    objects.delete()
+
+
+def mark_first_rows_responses_as_stale(
+    dataset_name: str, config_name: Optional[str] = None, split_name: Optional[str] = None
+):
+    objects = (
+        FirstRowsResponse.objects(dataset_name=dataset_name)
+        if config_name is None
+        else FirstRowsResponse.objects(dataset_name=dataset_name, config_name=config_name, split_name=split_name)
+    )
+    objects.update(stale=True, updated_at=get_datetime())
+
+
+# Note: we let the exceptions throw (ie DoesNotExist): it's the responsibility of the caller to manage them
+def get_first_rows_response(dataset_name: str, config_name: str, split_name: str) -> Tuple[Dict, HTTPStatus]:
+    first_rows_response = FirstRowsResponse.objects(
+        dataset_name=dataset_name, config_name=config_name, split_name=split_name
+    ).get()
+    return first_rows_response.response, first_rows_response.http_status
+
+
+def get_dataset_first_rows_response_splits(dataset_name: str) -> List[Tuple[str, str, str]]:
+    return [
+        (firstRowResponse.dataset_name, firstRowResponse.config_name, firstRowResponse.split_name)
+        for firstRowResponse in FirstRowsResponse.objects(dataset_name=dataset_name).only(
+            "dataset_name", "config_name", "split_name"
+        )
+    ]
+
+
+# /valid endpoint
+
+
+def get_valid_dataset_names() -> List[str]:
+    # a dataset is considered valid if:
+    # - the /splits response is valid
+    candidate_dataset_names = set(SplitsResponse.objects(http_status=HTTPStatus.OK).distinct("dataset_name"))
+    # - at least one of the /first-rows responses is valid
+    candidate_dataset_names_in_first_rows = set(
+        FirstRowsResponse.objects(http_status=HTTPStatus.OK).distinct("dataset_name")
+    )
+
+    candidate_dataset_names.intersection_update(candidate_dataset_names_in_first_rows)
+    # note that the list is sorted alphabetically for consistency
+    return sorted(candidate_dataset_names)
+
+
+class CountByHTTPStatus(TypedDict):
+    OK: int
+    BAD_REQUEST: int
+    INTERNAL_SERVER_ERROR: int
+
+
+def get_entries_count_by_status(entries: QuerySet[AnyResponse]) -> CountByHTTPStatus:
+    # ensure that all the statuses are present, even if equal to zero
+    # note: we repeat the values instead of looping on Status because we don't know how to get the types right in mypy
+    # result: CountByStatus = {s.value: entries(status=s.value).count() for s in Status} # <- doesn't work in mypy
+    # see https://stackoverflow.com/a/67292548/7351594
+    return {
+        "OK": entries(http_status=HTTPStatus.OK.value).count(),
+        "BAD_REQUEST": entries(http_status=HTTPStatus.BAD_REQUEST.value).count(),
+        "INTERNAL_SERVER_ERROR": entries(http_status=HTTPStatus.INTERNAL_SERVER_ERROR).count(),
+    }
+
+
+def get_splits_responses_count_by_status() -> CountByHTTPStatus:
+    # TODO: take the splits statuses into account?
+    return get_entries_count_by_status(SplitsResponse.objects)
+
+
+def get_first_rows_responses_count_by_status() -> CountByHTTPStatus:
+    return get_entries_count_by_status(FirstRowsResponse.objects)
+
+
+# only for the tests
+def _clean_database() -> None:
+    SplitsResponse.drop_collection()  # type: ignore
+    FirstRowsResponse.drop_collection()  # type: ignore
---
+++ libs/libcache/tests/test_simple_cache.py
@@ -0,0 +1,189 @@
+import pytest
+from mongoengine import DoesNotExist
+from pymongo.errors import DocumentTooLarge
+
+from libcache.simple_cache import (
+    HTTPStatus,
+    _clean_database,
+    connect_to_cache,
+    delete_first_rows_responses,
+    delete_splits_responses,
+    get_first_rows_response,
+    get_first_rows_responses_count_by_status,
+    get_splits_response,
+    get_splits_responses_count_by_status,
+    get_valid_dataset_names,
+    mark_first_rows_responses_as_stale,
+    mark_splits_responses_as_stale,
+    upsert_first_rows_response,
+    upsert_splits_response,
+)
+
+from ._utils import MONGO_CACHE_DATABASE, MONGO_URL
+
+
+@pytest.fixture(autouse=True, scope="module")
+def safe_guard() -> None:
+    if "test" not in MONGO_CACHE_DATABASE:
+        raise ValueError("Test must be launched on a test mongo database")
+
+
+@pytest.fixture(autouse=True, scope="module")
+def client() -> None:
+    connect_to_cache(database=MONGO_CACHE_DATABASE, host=MONGO_URL)
+
+
+@pytest.fixture(autouse=True)
+def clean_mongo_database() -> None:
+    _clean_database()
+
+
+def test_upsert_splits_response() -> None:
+    dataset_name = "test_dataset"
+    response = {"splits": [{"dataset_name": dataset_name, "config_name": "test_config", "split_name": "test_split"}]}
+    upsert_splits_response(dataset_name, response, HTTPStatus.OK)
+    response1, http_status = get_splits_response(dataset_name)
+    assert http_status == HTTPStatus.OK
+    assert response1 == response
+
+    # ensure it's idempotent
+    upsert_splits_response(dataset_name, response, HTTPStatus.OK)
+    (response2, _) = get_splits_response(dataset_name)
+    assert response2 == response1
+
+    mark_splits_responses_as_stale(dataset_name)
+    # we don't have access to the stale field
+    # we also don't have access to the updated_at field
+
+    delete_splits_responses(dataset_name)
+    with pytest.raises(DoesNotExist):
+        get_splits_response(dataset_name)
+
+    mark_splits_responses_as_stale(dataset_name)
+    with pytest.raises(DoesNotExist):
+        get_splits_response(dataset_name)
+
+
+def test_upsert_first_rows_response() -> None:
+    dataset_name = "test_dataset"
+    config_name = "test_config"
+    split_name = "test_split"
+    response = {"key": "value"}
+    upsert_first_rows_response(dataset_name, config_name, split_name, response, HTTPStatus.OK)
+    response1, http_status = get_first_rows_response(dataset_name, config_name, split_name)
+    assert http_status == HTTPStatus.OK
+    assert response1 == response
+
+    # ensure it's idempotent
+    upsert_first_rows_response(dataset_name, config_name, split_name, response, HTTPStatus.OK)
+    (response2, _) = get_first_rows_response(dataset_name, config_name, split_name)
+    assert response2 == response1
+
+    mark_first_rows_responses_as_stale(dataset_name)
+    mark_first_rows_responses_as_stale(dataset_name, config_name, split_name)
+    # we don't have access to the stale field
+    # we also don't have access to the updated_at field
+
+    upsert_first_rows_response(dataset_name, config_name, "test_split2", response, HTTPStatus.OK)
+    delete_first_rows_responses(dataset_name, config_name, "test_split2")
+    get_first_rows_response(dataset_name, config_name, split_name)
+
+    delete_first_rows_responses(dataset_name)
+    with pytest.raises(DoesNotExist):
+        get_first_rows_response(dataset_name, config_name, split_name)
+
+    mark_first_rows_responses_as_stale(dataset_name)
+    mark_first_rows_responses_as_stale(dataset_name, config_name, split_name)
+    with pytest.raises(DoesNotExist):
+        get_first_rows_response(dataset_name, config_name, split_name)
+
+
+def test_big_row() -> None:
+    # https://github.com/huggingface/datasets-server/issues/197
+    dataset_name = "test_dataset"
+    config_name = "test_config"
+    split_name = "test_split"
+    big_response = {"content": "a" * 100_000_000}
+    with pytest.raises(DocumentTooLarge):
+        upsert_first_rows_response(dataset_name, config_name, split_name, big_response, HTTPStatus.OK)
+
+
+def test_valid() -> None:
+    assert get_valid_dataset_names() == []
+
+    upsert_splits_response(
+        "test_dataset",
+        {"key": "value"},
+        HTTPStatus.OK,
+    )
+
+    assert get_valid_dataset_names() == []
+
+    upsert_first_rows_response(
+        "test_dataset",
+        "test_config",
+        "test_split",
+        {
+            "key": "value",
+        },
+        HTTPStatus.OK,
+    )
+
+    assert get_valid_dataset_names() == ["test_dataset"]
+
+    upsert_splits_response(
+        "test_dataset2",
+        {"key": "value"},
+        HTTPStatus.OK,
+    )
+
+    assert get_valid_dataset_names() == ["test_dataset"]
+
+    upsert_first_rows_response(
+        "test_dataset2",
+        "test_config2",
+        "test_split2",
+        {
+            "key": "value",
+        },
+        HTTPStatus.BAD_REQUEST,
+    )
+
+    assert get_valid_dataset_names() == ["test_dataset"]
+
+    upsert_first_rows_response(
+        "test_dataset2",
+        "test_config2",
+        "test_split3",
+        {
+            "key": "value",
+        },
+        HTTPStatus.OK,
+    )
+
+    assert get_valid_dataset_names() == ["test_dataset", "test_dataset2"]
+
+
+def test_count_by_status() -> None:
+    assert get_splits_responses_count_by_status() == {"OK": 0, "BAD_REQUEST": 0, "INTERNAL_SERVER_ERROR": 0}
+
+    upsert_splits_response(
+        "test_dataset2",
+        {"key": "value"},
+        HTTPStatus.OK,
+    )
+
+    assert get_splits_responses_count_by_status() == {"OK": 1, "BAD_REQUEST": 0, "INTERNAL_SERVER_ERROR": 0}
+    assert get_first_rows_responses_count_by_status() == {"OK": 0, "BAD_REQUEST": 0, "INTERNAL_SERVER_ERROR": 0}
+
+    upsert_first_rows_response(
+        "test_dataset",
+        "test_config",
+        "test_split",
+        {
+            "key": "value",
+        },
+        HTTPStatus.OK,
+    )
+
+    assert get_first_rows_responses_count_by_status() == {"OK": 1, "BAD_REQUEST": 0, "INTERNAL_SERVER_ERROR": 0}
