dcd92f415e829425a80c5928e59e03c1c9faccbb
Test User <sylvain.lesage@huggingface.co>
2022-08-08 15:41:36 +0000

    test: 💍 fix test

--- e2e/tests/test_80_valid_next.py
+++ e2e/tests/test_80_valid_next.py
@@ -7,7 +7,7 @@ def test_valid_after_datasets_processed(hf_dataset_repos_csv_data: DatasetRepos)
     response = get("/valid-next")
     assert response.status_code == 200, f"{response.status_code} - {response.text}"
     # at this moment various datasets have been processed (due to the alphabetic order of the test files)
-    valid = response.json()["valid-next"]
+    valid = response.json()["valid"]
     assert hf_dataset_repos_csv_data["public"] in valid, response.text
     assert hf_dataset_repos_csv_data["gated"] in valid, response.text
     assert hf_dataset_repos_csv_data["private"] not in valid, response.text
