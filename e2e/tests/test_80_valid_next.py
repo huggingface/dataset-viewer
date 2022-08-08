d9eb22f0338909c2563bd0a8e6a108ff3e8d9032
Test User <sylvain.lesage@huggingface.co>
2022-08-08 15:28:13 +0000

    test: 💍 test /valid-next and /is_valid-next

---
+++ e2e/tests/test_80_valid_next.py
@@ -0,0 +1,13 @@
+from .fixtures.hub import DatasetRepos
+from .utils import get
+
+
+def test_valid_after_datasets_processed(hf_dataset_repos_csv_data: DatasetRepos):
+    # this test ensures that the datasets processed successfully are present in /valid-next
+    response = get("/valid-next")
+    assert response.status_code == 200, f"{response.status_code} - {response.text}"
+    # at this moment various datasets have been processed (due to the alphabetic order of the test files)
+    valid = response.json()["valid-next"]
+    assert hf_dataset_repos_csv_data["public"] in valid, response.text
+    assert hf_dataset_repos_csv_data["gated"] in valid, response.text
+    assert hf_dataset_repos_csv_data["private"] not in valid, response.text
