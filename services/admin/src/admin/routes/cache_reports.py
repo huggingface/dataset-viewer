e86fec2bc3ec9d1a55b4a0f4d6aa8b054b0471d7
Test User <sylvain.lesage@huggingface.co>
2022-09-30 13:13:09 +0000

    feat: 🎸 add /cache-reports/features to the admin service
    also: refactor to reduce the code

--- services/admin/src/admin/routes/cache_reports.py
+++ services/admin/src/admin/routes/cache_reports.py
@@ -2,11 +2,12 @@
 # Copyright 2022 The HuggingFace Authors.
 
 import logging
-from typing import Optional
+from typing import Callable, Dict, Literal, Optional
 
 from libcache.simple_cache import (
     InvalidCursor,
     InvalidLimit,
+    get_cache_reports_features,
     get_cache_reports_first_rows,
     get_cache_reports_splits,
 )
@@ -27,17 +28,29 @@ from admin.utils import (
 logger = logging.getLogger(__name__)
 
 
-def create_cache_reports_first_rows_endpoint(
-    external_auth_url: Optional[str] = None, organization: Optional[str] = None
+EndpointName = Literal["features", "first-rows", "splits"]
+
+
+get_cache_reports: Dict[EndpointName, Callable] = {
+    "features": get_cache_reports_features,
+    "first-rows": get_cache_reports_first_rows,
+    "splits": get_cache_reports_splits,
+}
+
+
+def create_cache_reports_endpoint(
+    endpoint: EndpointName, external_auth_url: Optional[str] = None, organization: Optional[str] = None
 ) -> Endpoint:
-    async def cache_reports_first_rows_endpoint(request: Request) -> Response:
+    get_cache_reports = get_cache_reports_features if endpoint == "features" else get_cache_reports_first_rows
+
+    async def cache_reports_endpoint(request: Request) -> Response:
         try:
             cursor = request.query_params.get("cursor") or ""
-            logger.info(f"/cache-reports/first-rows, cursor={cursor}")
+            logger.info(f"/cache-reports/{endpoint}, cursor={cursor}")
             # if auth_check fails, it will raise an exception that will be caught below
             auth_check(external_auth_url=external_auth_url, request=request, organization=organization)
             try:
-                return get_json_ok_response(get_cache_reports_first_rows(cursor, CACHE_REPORTS_NUM_RESULTS))
+                return get_json_ok_response(get_cache_reports(cursor, CACHE_REPORTS_NUM_RESULTS))
             except InvalidCursor as e:
                 raise InvalidParameterError("Invalid cursor.") from e
             except InvalidLimit as e:
@@ -49,29 +62,4 @@ def create_cache_reports_first_rows_endpoint(
         except Exception:
             return get_json_admin_error_response(UnexpectedError("Unexpected error."))
 
-    return cache_reports_first_rows_endpoint
-
-
-def create_cache_reports_splits_endpoint(
-    external_auth_url: Optional[str] = None, organization: Optional[str] = None
-) -> Endpoint:
-    async def cache_reports_splits_endpoint(request: Request) -> Response:
-        try:
-            cursor = request.query_params.get("cursor") or ""
-            logger.info(f"/cache-reports/splits, cursor={cursor}")
-            # if auth_check fails, it will raise an exception that will be caught below
-            auth_check(external_auth_url=external_auth_url, request=request, organization=organization)
-            try:
-                return get_json_ok_response(get_cache_reports_splits(cursor, CACHE_REPORTS_NUM_RESULTS))
-            except InvalidCursor as e:
-                raise InvalidParameterError("Invalid cursor.") from e
-            except InvalidLimit as e:
-                raise UnexpectedError(
-                    "Invalid limit. CACHE_REPORTS_NUM_RESULTS must be a strictly positive integer."
-                ) from e
-        except AdminCustomError as e:
-            return get_json_admin_error_response(e)
-        except Exception:
-            return get_json_admin_error_response(UnexpectedError("Unexpected error."))
-
-    return cache_reports_splits_endpoint
+    return cache_reports_endpoint
