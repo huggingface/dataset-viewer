790286e52cbd7548f168a5a9173a1e5f11f1dae5
Test User <sylvain.lesage@huggingface.co>
2022-10-18 12:41:56 +0000

    fix: 🐛 change config field to queue_field

--- libs/libqueue/src/libqueue/worker.py
+++ libs/libqueue/src/libqueue/worker.py
@@ -16,43 +16,43 @@ logger = logging.getLogger(__name__)
 
 
 class Worker(ABC):
-    config: QueueConfig
+    queue_config: QueueConfig
 
     @property
     @abstractmethod
     def queue(self) -> Queue:
         pass
 
-    def __init__(self, config: QueueConfig) -> None:
-        self.config = config
+    def __init__(self, queue_config: QueueConfig) -> None:
+        self.queue_config = queue_config
 
     def has_memory(self) -> bool:
-        if self.config.max_memory_pct <= 0:
+        if self.queue_config.max_memory_pct <= 0:
             return True
         virtual_memory_used: int = virtual_memory().used  # type: ignore
         virtual_memory_total: int = virtual_memory().total  # type: ignore
         percent = (swap_memory().used + virtual_memory_used) / (swap_memory().total + virtual_memory_total)
-        ok = percent < self.config.max_memory_pct
+        ok = percent < self.queue_config.max_memory_pct
         if not ok:
             logger.info(
-                f"memory usage (RAM + SWAP) is too high: {percent:.0f}% - max is {self.config.max_memory_pct}%"
+                f"memory usage (RAM + SWAP) is too high: {percent:.0f}% - max is {self.queue_config.max_memory_pct}%"
             )
         return ok
 
     def has_cpu(self) -> bool:
-        if self.config.max_load_pct <= 0:
+        if self.queue_config.max_load_pct <= 0:
             return True
         load_pct = max(getloadavg()[:2]) / cpu_count() * 100
         # ^ only current load and 5m load. 15m load is not relevant to decide to launch a new job
-        ok = load_pct < self.config.max_load_pct
+        ok = load_pct < self.queue_config.max_load_pct
         if not ok:
-            logger.info(f"cpu load is too high: {load_pct:.0f}% - max is {self.config.max_load_pct}%")
+            logger.info(f"cpu load is too high: {load_pct:.0f}% - max is {self.queue_config.max_load_pct}%")
         return ok
 
     def sleep(self) -> None:
         jitter = 0.75 + random.random() / 2  # nosec
         # ^ between 0.75 and 1.25
-        duration = self.config.sleep_seconds * jitter
+        duration = self.queue_config.sleep_seconds * jitter
         logger.debug(f"sleep during {duration:.2f} seconds")
         time.sleep(duration)
 
