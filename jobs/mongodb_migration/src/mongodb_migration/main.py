cf65b25654d77b9a0cedd30cf82cee13ef075647
Test User <sylvain.lesage@huggingface.co>
2022-11-18 08:55:34 +0000

    fix: 🐛 the migration script returns exit code "1" if error
    So that the hook will stop the update of the Helm chart. If not, the
    code will be updated (and expects the new database) while the database
    migration has failed

--- jobs/mongodb_migration/src/mongodb_migration/main.py
+++ jobs/mongodb_migration/src/mongodb_migration/main.py
@@ -4,11 +4,16 @@
 from mongodb_migration.collector import MigrationsCollector
 from mongodb_migration.config import JobConfig
 from mongodb_migration.plan import Plan
+import sys
 
 if __name__ == "__main__":
     job_config = JobConfig()
     collected_migrations = MigrationsCollector().get_migrations()
-    Plan(collected_migrations=collected_migrations).execute()
+    try:
+        Plan(collected_migrations=collected_migrations).execute()
+        sys.exit(0)
+    except Exception:
+        sys.exit(1)
 
 # See:
 #  https://blog.appsignal.com/2020/04/14/dissecting-rails-migrationsl.html
