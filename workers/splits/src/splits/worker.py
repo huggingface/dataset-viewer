6108cb39565935c476e34940d4dc0b7ef3acf673
Test User <sylvain.lesage@huggingface.co>
2022-11-25 17:49:29 +0000

    feat: 🎸 update libcommon to 0.4.2

--- workers/splits/src/splits/worker.py
+++ workers/splits/src/splits/worker.py
@@ -4,79 +4,174 @@
 import importlib.metadata
 import logging
 from http import HTTPStatus
-from typing import Optional
+from typing import Any, Dict, List, Literal, Mapping, Optional, TypedDict, Union
 
-from libcommon.processing_steps import ProcessingStep, first_rows_step, splits_step
-from libcommon.simple_cache import (
-    delete_response,
-    get_dataset_response_ids,
-    get_response_without_content,
-    upsert_response,
+from datasets import (
+    DatasetInfo,
+    get_dataset_config_info,
+    get_dataset_config_names,
+    get_dataset_split_names,
 )
+from datasets.data_files import EmptyDatasetError as _EmptyDatasetError
+from libcommon.exceptions import CustomError
+from libcommon.processing_steps import first_rows_step, splits_step
+from libcommon.simple_cache import delete_response, get_dataset_response_ids
 from libcommon.worker import Queue, Worker
 
 from splits.config import WorkerConfig
-from splits.response import compute_splits_response, get_dataset_git_revision
-from splits.utils import DatasetNotFoundError, UnexpectedError, WorkerCustomError
+
+SplitsWorkerErrorCode = Literal[
+    "EmptyDatasetError",
+    "SplitsNamesError",
+]
+
+
+class SplitWorkerError(CustomError):
+    """Base class for worker exceptions."""
+
+    def __init__(
+        self,
+        message: str,
+        status_code: HTTPStatus,
+        code: SplitsWorkerErrorCode,
+        cause: Optional[BaseException] = None,
+        disclose_cause: bool = False,
+    ):
+        super().__init__(
+            message=message, status_code=status_code, code=str(code), cause=cause, disclose_cause=disclose_cause
+        )
+
+
+class SplitsNamesError(SplitWorkerError):
+    """Raised when the split names could not be fetched."""
+
+    def __init__(self, message: str, cause: Optional[BaseException] = None):
+        super().__init__(message, HTTPStatus.INTERNAL_SERVER_ERROR, "SplitsNamesError", cause, True)
+
+
+class EmptyDatasetError(SplitWorkerError):
+    """Raised when the dataset has no data."""
+
+    def __init__(self, message: str, cause: Optional[BaseException] = None):
+        super().__init__(message, HTTPStatus.INTERNAL_SERVER_ERROR, "EmptyDatasetError", cause, True)
+
+
+class SplitFullName(TypedDict):
+    dataset: str
+    config: str
+    split: str
+
+
+class SplitItem(SplitFullName):
+    num_bytes: Optional[int]
+    num_examples: Optional[int]
+
+
+class SplitsResponseContent(TypedDict):
+    splits: List[SplitItem]
+
+
+def get_dataset_split_full_names(dataset: str, use_auth_token: Union[bool, str, None] = False) -> List[SplitFullName]:
+    """Get the list of splits full names (split and config) for a dataset.
+
+    Args:
+        dataset (str): A dataset name. If the repository is namespaced (a user or an organization), the namespace and
+          the dataset name are separated with a slash (`/`), for example: `user/dataset`.
+        use_auth_token (Union[bool, str, None], optional): user token. It allows to retrieve the splits for gated
+          datasets. Defaults to False (no authentication).
+
+    Returns:
+        List[SplitFullName]: a list of splits full names: objects with the keys `dataset`, `config` and `split`. They
+          are sorted alphabetically by configuration (config), but the splits order for a given configuration is
+          preserved.
+    """
+    logging.info(f"get dataset '{dataset}' split full names")
+    return [
+        {"dataset": dataset, "config": config, "split": split}
+        for config in sorted(get_dataset_config_names(path=dataset, use_auth_token=use_auth_token))
+        for split in get_dataset_split_names(path=dataset, config_name=config, use_auth_token=use_auth_token)
+    ]
+
+
+def compute_splits_response(
+    dataset: str,
+    hf_token: Optional[str] = None,
+) -> SplitsResponseContent:
+    """
+    Get the response of /splits for one specific dataset on huggingface.co.
+    Dataset can be private or gated if you pass an acceptable token.
+
+    It is assumed that the dataset exist and can be accessed using the token.
+
+    Args:
+        dataset (`str`):
+            A namespace (user or an organization) and a repo name separated
+            by a `/`.
+        hf_endpoint (`str`):
+            The Hub endpoint (for example: "https://huggingface.co")
+        hf_token (`str`, *optional*):
+            An authentication token (See https://huggingface.co/settings/token)
+    Returns:
+        `SplitsResponseResult`: An object with the splits_response
+          (list of splits names) and the dataset_git_revision (sha) if any.
+    <Tip>
+    Raises the following errors:
+        - [`~worker.exceptions.DatasetNotFoundError`]
+          If the repository to download from cannot be found. This may be because it doesn't exist,
+          or because it is set to `private` and you do not have access.
+        - [`~worker.exceptions.SplitsNamesError`]
+          If the list of splits could not be obtained using the datasets library.
+    </Tip>
+    """
+    logging.info(f"get splits for dataset={dataset}")
+    use_auth_token: Union[bool, str, None] = hf_token if hf_token is not None else False
+    # get the list of splits
+    try:
+        split_full_names = get_dataset_split_full_names(dataset=dataset, use_auth_token=use_auth_token)
+    except _EmptyDatasetError as err:
+        raise EmptyDatasetError("The dataset is empty.", cause=err) from err
+    except Exception as err:
+        raise SplitsNamesError("Cannot get the split names for the dataset.", cause=err) from err
+    # get the number of bytes and examples for each split
+    config_info: Dict[str, DatasetInfo] = {}
+    split_items: List[SplitItem] = []
+    for split_full_name in split_full_names:
+        dataset = split_full_name["dataset"]
+        config = split_full_name["config"]
+        split = split_full_name["split"]
+        try:
+            if config not in config_info:
+                config_info[config] = get_dataset_config_info(
+                    path=dataset,
+                    config_name=config,
+                    use_auth_token=use_auth_token,
+                )
+            info = config_info[config]
+            num_bytes = info.splits[split].num_bytes if info.splits else None
+            num_examples = info.splits[split].num_examples if info.splits else None
+        except Exception:
+            num_bytes = None
+            num_examples = None
+        split_items.append(
+            {
+                "dataset": dataset,
+                "config": config,
+                "split": split,
+                "num_bytes": num_bytes,
+                "num_examples": num_examples,
+            }
+        )
+    return {"splits": split_items}
 
 
 class SplitsWorker(Worker):
-    config: WorkerConfig
-    processing_step: ProcessingStep
-
     def __init__(self, worker_config: WorkerConfig):
-        super().__init__(queue_config=worker_config.queue, version=importlib.metadata.version(__package__))
-
-        # self._queues = Queues(max_jobs_per_namespace=worker_config.queue.max_jobs_per_namespace)
-        self.config = worker_config
-        self.processing_step = splits_step
-
-    @property
-    def queue(self):
-        return Queue(
-            type=self.processing_step.job_type, max_jobs_per_namespace=self.config.queue.max_jobs_per_namespace
+        super().__init__(
+            processing_step=splits_step,
+            common_config=worker_config.common,
+            queue_config=worker_config.queue,
+            version=importlib.metadata.version(__package__),
         )
-
-    def should_skip_job(
-        self, dataset: str, config: Optional[str] = None, split: Optional[str] = None, force: bool = False
-    ) -> bool:
-        """Return True if the job should be skipped, False otherwise.
-
-        The job must be skipped if:
-        - force is False
-        - and a cache entry exists for the dataset
-        - and the result was successful
-        - and it has been created with the same major version of the worker
-        - and it has been created with the exact same git commit of the dataset repository
-
-        Args:
-            dataset (:obj:`str`): The name of the dataset.
-            config (:obj:`str`, `optional`): The name of the configuration.
-            split (:obj:`str`, `optional`): The name of the split.
-            force (:obj:`bool`, `optional`, defaults to :obj:`False`): Whether to force the job to be run.
-
-        Returns:
-            :obj:`bool`: True if the job should be skipped, False otherwise.
-        """
-        if force:
-            return False
-        try:
-            cached_response = get_response_without_content(
-                kind=self.processing_step.cache_kind, dataset=dataset, config=config, split=split
-            )
-            dataset_git_revision = get_dataset_git_revision(
-                dataset=dataset, hf_endpoint=self.config.common.hf_endpoint, hf_token=self.config.common.hf_token
-            )
-            return (
-                # TODO: use "error_code" to decide if the job should be skipped (ex: retry if temporary error)
-                cached_response["http_status"] == HTTPStatus.OK
-                and cached_response["worker_version"] is not None
-                and self.compare_major_version(cached_response["worker_version"]) == 0
-                and cached_response["dataset_git_revision"] is not None
-                and cached_response["dataset_git_revision"] == dataset_git_revision
-            )
-        except Exception:
-            return False
 
     def compute(
         self,
@@ -84,74 +179,28 @@ class SplitsWorker(Worker):
         config: Optional[str] = None,
         split: Optional[str] = None,
         force: bool = False,
-    ) -> bool:
-        dataset_git_revision = None
-        try:
-            dataset_git_revision = get_dataset_git_revision(
-                dataset=dataset, hf_endpoint=self.config.common.hf_endpoint, hf_token=self.config.common.hf_token
-            )
-        except DatasetNotFoundError:
-            logging.debug(f"the dataset={dataset} could not be found, don't update the cache")
-            return False
-        if dataset_git_revision is None:
-            logging.debug(f"the dataset={dataset} has no git revision, don't update the cache")
-            return False
+    ) -> Mapping[str, Any]:
+        content = compute_splits_response(dataset=dataset, hf_token=self.common_config.hf_token)
 
-        try:
-            splits_response_result = compute_splits_response(
-                dataset=dataset, hf_endpoint=self.config.common.hf_endpoint, hf_token=self.config.common.hf_token
-            )
-            content = splits_response_result["splits_response"]
-            if splits_response_result["dataset_git_revision"] != dataset_git_revision:
-                raise UnexpectedError("The dataset git revision has changed during the job")
-            upsert_response(
-                kind=self.processing_step.cache_kind,
-                dataset=dataset,
-                config=config,
-                split=split,
-                content=dict(content),
-                http_status=HTTPStatus.OK,
-                worker_version=self.version,
-                dataset_git_revision=dataset_git_revision,
-            )
-            logging.debug(f"dataset={dataset} is valid, cache updated")
+        # TODO: create a "hook" instead, so that we don't refer to first_rows here
+        new_splits = [(s["dataset"], s["config"], s["split"]) for s in content["splits"]]
+        # remove obsolete first-rows responses from the cache
+        first_rows_responses_in_cache = [
+            (s["dataset"], s["config"], s["split"])
+            for s in get_dataset_response_ids(dataset=dataset)
+            if s["kind"] == first_rows_step.cache_kind
+        ]
+        first_rows_responses_to_delete = [s for s in first_rows_responses_in_cache if s not in new_splits]
+        for d, c, s in first_rows_responses_to_delete:
+            delete_response(kind=first_rows_step.cache_kind, dataset=d, config=c, split=s)
+        logging.debug(
+            f"{len(first_rows_responses_to_delete)} 'first-rows' responses deleted from the cache for obsolete"
+            f" splits of dataset={dataset}"
+        )
+        # compute the 'first-rows' responses for the new splits
+        for d, c, s in new_splits:
+            # we force the refresh of the /first_rows responses if the /splits refresh was forced
+            Queue(type=first_rows_step.job_type).add_job(dataset=d, config=c, split=s, force=force)
+        logging.debug(f"{len(new_splits)} 'first-rows' jobs added for the splits of dataset={dataset}")
 
-            new_splits = [(s["dataset"], s["config"], s["split"]) for s in content["splits"]]
-            # remove obsolete first-rows responses from the cache
-            first_rows_responses_in_cache = [
-                (s["dataset"], s["config"], s["split"])
-                for s in get_dataset_response_ids(dataset=dataset)
-                if s["kind"] == first_rows_step.cache_kind
-            ]
-            first_rows_responses_to_delete = [s for s in first_rows_responses_in_cache if s not in new_splits]
-            for d, c, s in first_rows_responses_to_delete:
-                delete_response(kind=first_rows_step.cache_kind, dataset=d, config=c, split=s)
-            logging.debug(
-                f"{len(first_rows_responses_to_delete)} 'first-rows' responses deleted from the cache for obsolete"
-                f" splits of dataset={dataset}"
-            )
-            # compute the 'first-rows' responses for the new splits
-            for d, c, s in new_splits:
-                # we force the refresh of the /first_rows responses if the /splits refresh was forced
-                Queue(type=first_rows_step.job_type).add_job(dataset=d, config=c, split=s, force=force)
-            logging.debug(f"{len(new_splits)} 'first-rows' jobs added for the splits of dataset={dataset}")
-            return True
-        except DatasetNotFoundError:
-            logging.debug(f"the dataset={dataset} could not be found, don't update the cache")
-            return False
-        except Exception as err:
-            e = err if isinstance(err, WorkerCustomError) else UnexpectedError(str(err), err)
-            upsert_response(
-                kind=self.processing_step.cache_kind,
-                dataset=dataset,
-                config=config,
-                split=split,
-                content=dict(e.as_response()),
-                http_status=e.status_code,
-                error_code=e.code,
-                details=dict(e.as_response_with_cause()),
-                worker_version=self.version,
-                dataset_git_revision=dataset_git_revision,
-            )
-            logging.debug(f"splits response for dataset={dataset} had an error, cache updated")
-            return False
+        return content
--- workers/splits/tests/test_worker.py
+++ workers/splits/tests/test_worker.py
@@ -4,11 +4,14 @@
 from http import HTTPStatus
 
 import pytest
+from libcommon.exceptions import CustomError
 from libcommon.queue import _clean_queue_database
 from libcommon.simple_cache import DoesNotExist, _clean_cache_database, get_response
 
 from splits.config import WorkerConfig
-from splits.worker import SplitsWorker
+from splits.worker import SplitsWorker, compute_splits_response
+
+from .fixtures.hub import HubDatasets
 
 
 @pytest.fixture(autouse=True)
@@ -32,14 +35,14 @@ def should_skip_job(worker: SplitsWorker, hub_public_csv: str) -> None:
     dataset = hub_public_csv
     assert worker.should_skip_job(dataset=dataset) is False
     # we add an entry to the cache
-    worker.compute(dataset=dataset)
+    worker.process(dataset=dataset)
     assert worker.should_skip_job(dataset=dataset) is True
     assert worker.should_skip_job(dataset=dataset, force=True) is False
 
 
-def test_compute(worker: SplitsWorker, hub_public_csv: str) -> None:
+def test_process(worker: SplitsWorker, hub_public_csv: str) -> None:
     dataset = hub_public_csv
-    assert worker.compute(dataset=dataset) is True
+    assert worker.process(dataset=dataset) is True
     cached_response = get_response(kind=worker.processing_step.cache_kind, dataset=hub_public_csv)
     assert cached_response["http_status"] == HTTPStatus.OK
     assert cached_response["error_code"] is None
@@ -54,7 +57,7 @@ def test_compute(worker: SplitsWorker, hub_public_csv: str) -> None:
 
 def test_doesnotexist(worker: SplitsWorker) -> None:
     dataset = "doesnotexist"
-    assert worker.compute(dataset=dataset) is False
+    assert worker.process(dataset=dataset) is False
     with pytest.raises(DoesNotExist):
         get_response(kind=worker.processing_step.cache_kind, dataset=dataset)
 
@@ -63,3 +66,52 @@ def test_process_job(worker: SplitsWorker, hub_public_csv: str) -> None:
     worker.queue.add_job(dataset=hub_public_csv)
     result = worker.process_next_job()
     assert result is True
+
+
+@pytest.mark.parametrize(
+    "name,use_token,error_code,cause",
+    [
+        ("public", False, None, None),
+        ("audio", False, None, None),
+        ("gated", True, None, None),
+        ("private", True, None, None),
+        ("empty", False, "EmptyDatasetError", "EmptyDatasetError"),
+        # should we really test the following cases?
+        # The assumption is that the dataset exists and is accessible with the token
+        ("does_not_exist", False, "SplitsNamesError", "FileNotFoundError"),
+        ("gated", False, "SplitsNamesError", "FileNotFoundError"),
+        ("private", False, "SplitsNamesError", "FileNotFoundError"),
+    ],
+)
+def test_compute_splits_response_simple_csv(
+    hub_datasets: HubDatasets, name: str, use_token: bool, error_code: str, cause: str, worker_config: WorkerConfig
+) -> None:
+    dataset = hub_datasets[name]["name"]
+    expected_splits_response = hub_datasets[name]["splits_response"]
+    if error_code is None:
+        result = compute_splits_response(
+            dataset=dataset,
+            hf_token=worker_config.common.hf_token if use_token else None,
+        )
+        assert result == expected_splits_response
+        return
+
+    with pytest.raises(CustomError) as exc_info:
+        compute_splits_response(
+            dataset=dataset,
+            hf_token=worker_config.common.hf_token if use_token else None,
+        )
+    assert exc_info.value.code == error_code
+    if cause is None:
+        assert exc_info.value.disclose_cause is False
+        assert exc_info.value.cause_exception is None
+    else:
+        assert exc_info.value.disclose_cause is True
+        assert exc_info.value.cause_exception == cause
+        response = exc_info.value.as_response()
+        assert set(response.keys()) == {"error", "cause_exception", "cause_message", "cause_traceback"}
+        response_dict = dict(response)
+        # ^ to remove mypy warnings
+        assert response_dict["cause_exception"] == cause
+        assert isinstance(response_dict["cause_traceback"], list)
+        assert response_dict["cause_traceback"][0] == "Traceback (most recent call last):\n"
