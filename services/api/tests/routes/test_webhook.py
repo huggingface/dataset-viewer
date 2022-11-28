6a899d8d8f71b0edb7b75184fca252e1b32d1e54
Test User <sylvain.lesage@huggingface.co>
2022-11-28 20:59:58 +0000

    test: 💍 add some tests for the webhook

---
+++ services/api/tests/routes/test_webhook.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 The HuggingFace Authors.
+
+import pytest
+from typing import Any, Mapping
+from api.routes.webhook import parse_payload
+
+
+@pytest.mark.parametrize(
+    "payload,raises",
+    [
+        ({"event": "add", "repo": {"type": "dataset", "name": "webhook-test", "gitalyUid": "123"}}, False),
+        (
+            {
+                "event": "move",
+                "movedTo": "webhook-test",
+                "repo": {"type": "dataset", "name": "previous-name", "gitalyUid": "123"},
+            },
+            False,
+        ),
+        ({"event": "add", "repo": {"type": "dataset", "name": "webhook-test"}}, False),
+        ({"event": "doesnotexist", "repo": {"type": "dataset", "name": "webhook-test", "gitalyUid": "123"}}, True),
+    ],
+)
+def test_parse_payload(
+    payload: Mapping[str, Any],
+    raises: bool,
+) -> None:
+    if raises:
+        with pytest.raises(Exception):
+            parse_payload(payload)
+    else:
+        parse_payload(payload)
