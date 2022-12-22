64e3554ebefc9a5c44c63da21e526add3e27733a
Test User <sylvain.lesage@huggingface.co>
2022-12-22 12:55:50 +0000

    feat: 🎸 update libcommon

--- jobs/mongodb_migration/src/mongodb_migration/config.py
+++ jobs/mongodb_migration/src/mongodb_migration/config.py
@@ -1,36 +1,44 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2022 The HuggingFace Authors.
 
+from dataclasses import dataclass
+
 from environs import Env
 from libcommon.config import CacheConfig, CommonConfig, QueueConfig
 
 from mongodb_migration.database_migrations import connect_to_database
 
 
+@dataclass
 class MongodbMigrationConfig:
-    mongo_database: str
-    mongo_url: str
+    mongo_database: str = "datasets_server_maintenance"
+    mongo_url: str = "mongodb://localhost:27017"
 
-    def __init__(self):
-        env = Env(expand_vars=True)
-        with env.prefixed("MONGODB_MIGRATION_"):
-            self.mongo_database = env.str(name="MONGO_DATABASE", default="datasets_server_maintenance")
-            self.mongo_url = env.str(name="MONGO_URL", default="mongodb://localhost:27017")
-        self.setup()
-
-    def setup(self):
+    def __post_init__(self):
         connect_to_database(database=self.mongo_database, host=self.mongo_url)
 
+    @staticmethod
+    def from_env() -> "MongodbMigrationConfig":
+        env = Env(expand_vars=True)
+        with env.prefixed("MONGODB_MIGRATION_"):
+            return MongodbMigrationConfig(
+                mongo_database=env.str(name="MONGO_DATABASE", default=None),
+                mongo_url=env.str(name="MONGO_URL", default=None),
+            )
 
+
+@dataclass
 class JobConfig:
     cache: CacheConfig
     common: CommonConfig
     mongodb_migration: MongodbMigrationConfig
     queue: QueueConfig
 
-    def __init__(self):
-        # First process the common configuration to setup the logging
-        self.common = CommonConfig()
-        self.cache = CacheConfig()
-        self.mongodb_migration = MongodbMigrationConfig()
-        self.queue = QueueConfig()
+    @staticmethod
+    def from_env() -> "JobConfig":
+        return JobConfig(
+            common=CommonConfig.from_env(),
+            cache=CacheConfig.from_env(),
+            mongodb_migration=MongodbMigrationConfig.from_env(),
+            queue=QueueConfig.from_env(),
+        )
