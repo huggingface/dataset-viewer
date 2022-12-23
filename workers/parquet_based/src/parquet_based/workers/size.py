d2cfd80ad9d2f280e3c7788b42e1a66d8d72f602
Test User <sylvain.lesage@huggingface.co>
2022-12-23 21:45:50 +0000

    feat: 🎸 first test with parquet based worker
    it reads the remote parquet files (from the Hub) with hffs and pyarrow.

---
+++ workers/parquet_based/src/parquet_based/workers/size.py
@@ -0,0 +1,116 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 The HuggingFace Authors.
+
+from dataclasses import asdict, dataclass
+from typing import Any, List, Mapping, TypedDict
+import hffs
+import pyarrow.parquet as pq
+
+from parquet_based.workers._parquet_based_worker import ParquetBasedWorker, ParquetFile
+
+
+@dataclass
+class ParquetFileWithSize(ParquetFile):
+    dataset: str
+    config: str
+    split: str
+    url: str
+    filename: str
+    size: int
+    num_bytes: int = 0
+    num_rows: int = 0
+    num_columns: int = 0
+
+    def __post_init__(self):
+        self.num_bytes = self.size
+
+    def compute_size(self) -> "ParquetFileWithSize":
+        REVISION = "refs/convert/parquet"
+        fs = hffs.HfFileSystem(self.dataset, repo_type="dataset", revision=REVISION)
+        metadata = pq.read_metadata(f"{self.config}/{self.filename}", filesystem=fs)
+        # ^ are we streaming to only read the metadata in the footer, or is the whole parquet file read?
+        self.num_rows = metadata.num_rows
+        self.num_columns = metadata.num_columns
+        return self
+
+    def as_item(self) -> "ParquetFileItem":
+        return {
+            "dataset": self.dataset,
+            "config": self.config,
+            "split": self.split,
+            "url": self.url,
+            "filename": self.filename,
+            "num_bytes": self.num_bytes,
+            "num_rows": self.num_rows,
+            "num_columns": self.num_columns,
+        }
+
+
+class ParquetFileItem(TypedDict):
+    dataset: str
+    config: str
+    split: str
+    url: str
+    filename: str
+    num_bytes: int
+    num_rows: int
+    num_columns: int
+
+
+class SplitItem(TypedDict):
+    dataset: str
+    config: str
+    split: str
+    num_parquet_files: int
+    num_bytes: int
+    num_rows: int
+    num_columns: int
+
+
+class ConfigItem(TypedDict):
+    dataset: str
+    config: str
+    num_splits: int
+    num_parquet_files: int
+    num_bytes: int
+    num_rows: int
+    # num_columns: int
+
+
+class DatasetItem(TypedDict):
+    dataset: str
+    num_configs: int
+    num_splits: int
+    num_parquet_files: int
+    num_bytes: int
+    num_rows: int
+    # num_columns: int
+
+
+class SizeResponseContent(TypedDict):
+    # dataset: DatasetItem
+    # configs: List[ConfigItem]
+    # splits: List[SplitItem]
+    parquet_files: List[ParquetFileItem]
+
+
+def get_size_response_content(parquet_files: List[ParquetFile]) -> SizeResponseContent:
+    # get the number of sample of each parquet file, and compute the response
+    parquet_files_with_size: List[ParquetFileWithSize] = [
+        ParquetFileWithSize(**(asdict(parquet_file))).compute_size() for parquet_file in parquet_files
+    ]
+    # return the list of splits parquet files with their size and number of samples
+    return {"parquet_files": [parquet_file_with_size.as_item() for parquet_file_with_size in parquet_files_with_size]}
+
+
+class SizeWorker(ParquetBasedWorker):
+    @staticmethod
+    def get_job_type() -> str:
+        return "/size"
+
+    @staticmethod
+    def get_version() -> str:
+        return "1.0.0"
+
+    def compute(self) -> Mapping[str, Any]:
+        return get_size_response_content(parquet_files=self.parquet_files)
