hugoross3000/datasets-server

Small tweaks on Helm charts

chart/templates/_envCommon.tpl
@@ -10,7 +10,11 @@
   {{- if .Values.secrets.token.fromSecret }}
   valueFrom:
     secretKeyRef:
+      {{- if eq .Values.secrets.token.secretName "" }}
+      name: {{ .Release.Name }}-datasets-server-app-token
+      {{- else }}
       name: {{ .Values.secrets.token.secretName | quote }}
+      {{- end }}
       key: HF_TOKEN
       optional: false
   {{- else }}

huggingface/dataset-viewer

chart/templates/_envCommon.tpl
@@ -10,7 +10,11 @@
   {{- if .Values.secrets.appHfToken.fromSecret }}
   valueFrom:
     secretKeyRef:
+      {{- if eq .Values.secrets.appHfToken.secretName "" }}
+      name: {{ .Release.Name }}-datasets-server-app-token
+      {{- else }}
       name: {{ .Values.secrets.appHfToken.secretName | quote }}
+      {{- end }}
       key: HF_TOKEN
       optional: false
   {{- else }}