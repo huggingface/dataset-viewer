718703d79c5b29cfca1cde583138d0ef70ff950c
Test User <sylvain.lesage@huggingface.co>
2023-01-03 10:00:46 +0000

    feat: 🎸 add ochestrator to docker compose + nginx reverse proxy
    note that we specifically allow http + ws (see
    https://nginx.org/en/docs/http/websocket.html)

--- tools/DockerRemoteImages.mk
+++ tools/DockerRemoteImages.mk
@@ -1,4 +1,5 @@
 export IMAGE_REVERSE_PROXY := $(shell jq -r '.dockerImage.reverseProxy' ${DOCKER_IMAGES})
 export IMAGE_SERVICE_ADMIN := $(shell jq -r '.dockerImage.services.admin' ${DOCKER_IMAGES})
 export IMAGE_SERVICE_API := $(shell jq -r '.dockerImage.services.api' ${DOCKER_IMAGES})
+export IMAGE_SERVICE_ORCHESTRATOR := $(shell jq -r '.dockerImage.services.orchestrator' ${DOCKER_IMAGES})
 export IMAGE_WORKER_DATASETS_BASED := $(shell jq -r '.dockerImage.workers.datasets_based' ${DOCKER_IMAGES})
