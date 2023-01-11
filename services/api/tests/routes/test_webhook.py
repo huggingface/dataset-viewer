d0ce1acf003f2a17d477b6f3a9def13cdea2f374
Sylvain Lesage <sylvain.lesage@huggingface.co>
2023-01-11 16:01:11 +0100

    fix: 🐛 only check webhook payload for what we are interested in (#676)
    * fix: 🐛 only check webhook payload for what we are interested in
    
    checking the payload in details (eg if the URL field is well formed)
    while not using it afterward is not useful. It even lead to breaking the
    webhook after https://github.com/huggingface/moon-landing/pull/4477
    (internal link) where the "url" field format had changed.
    
    * test: 💍 add two unit tests
    
    * feat: 🎸 update docker image

--- services/api/tests/routes/test_webhook.py
+++ services/api/tests/routes/test_webhook.py
@@ -22,6 +22,32 @@ from api.routes.webhook import parse_payload
         ),
         ({"event": "add", "repo": {"type": "dataset", "name": "webhook-test"}}, False),
         ({"event": "doesnotexist", "repo": {"type": "dataset", "name": "webhook-test", "gitalyUid": "123"}}, True),
+        (
+            {
+                "event": "update",
+                "scope": "repo.content",
+                "repo": {
+                    "type": "dataset",
+                    "name": "AresEkb/prof_standards_sbert_large_mt_nlu_ru",
+                    "id": "63bab13ae0f4fee16cebf084",
+                    "private": False,
+                    "url": {
+                        "web": "https://huggingface.co/datasets/AresEkb/prof_standards_sbert_large_mt_nlu_ru",
+                        "api": "https://huggingface.co/api/datasets/AresEkb/prof_standards_sbert_large_mt_nlu_ru",
+                    },
+                    "headSha": "c926e6ce93cbd5a6eaf0895abd48776cc5bae638",
+                    "gitalyUid": "c5afeca93171cfa1f6c138ef683df4a53acffd8c86283ab8e7e338df369d2fff",
+                    "authorId": "6394b8740b746ac6a969bd51",
+                    "tags": [],
+                },
+                "webhook": {"id": "632c22b3df82fca9e3b46154", "version": 2},
+            },
+            False,
+        ),
+        (
+            {"event": "update", "repo": {"type": "dataset", "name": "AresEkb/prof_standards_sbert_large_mt_nlu_ru"}},
+            False,
+        ),
     ],
 )
 def test_parse_payload(
