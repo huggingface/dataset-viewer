c6320b788a3083012ef9c57ccb4dc8b17b87df93
Test User <sylvain.lesage@huggingface.co>
2023-01-16 14:16:16 +0000

    ci: 🎡 simplify pip-audit
    use only one definition. Also: remove the ignored vulnerabilities, since
    the dependencies have been updated

--- tools/PythonAudit.mk
+++
@@ -1,4 +0,0 @@
-.PHONY: pip-audit
-pip-audit:
-	bash -c "poetry run pip-audit --ignore-vuln GHSA-hcpj-qp55-gfph -r <(poetry export -f requirements.txt --with dev | sed '/^pymongo==/,+109 d')"
-# ^ we remove problematic lines to have a working pip-audit. See https://github.com/pypa/pip-audit/issues/84#issuecomment-1326203111 for "requests"
