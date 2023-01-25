ef6790b6b886cdd030ae1c90ea2ddcaf89605d82
Test User <sylvain.lesage@huggingface.co>
2023-01-25 21:18:44 +0000

    refactor: 💡 no need to use elif

--- workers/datasets_based/src/datasets_based/worker_factory.py
+++ workers/datasets_based/src/datasets_based/worker_factory.py
@@ -26,30 +26,29 @@ class DatasetBasedWorkerFactory(WorkerFactory):
             return ConfigNamesWorker(job_info=job_info, app_config=self.app_config)
         if job_type == SplitsWorker.get_job_type():
             return SplitsWorker(job_info=job_info, app_config=self.app_config)
-        elif job_type == FirstRowsWorker.get_job_type():
+        if job_type == FirstRowsWorker.get_job_type():
             return FirstRowsWorker(
                 job_info=job_info, app_config=self.app_config, first_rows_config=FirstRowsConfig.from_env()
             )
-        elif job_type == ParquetAndDatasetInfoWorker.get_job_type():
+        if job_type == ParquetAndDatasetInfoWorker.get_job_type():
             return ParquetAndDatasetInfoWorker(
                 job_info=job_info,
                 app_config=self.app_config,
                 parquet_and_dataset_info_config=ParquetAndDatasetInfoConfig.from_env(),
             )
-        elif job_type == ParquetWorker.get_job_type():
+        if job_type == ParquetWorker.get_job_type():
             return ParquetWorker(job_info=job_info, app_config=self.app_config)
-        elif job_type == DatasetInfoWorker.get_job_type():
+        if job_type == DatasetInfoWorker.get_job_type():
             return DatasetInfoWorker(job_info=job_info, app_config=self.app_config)
-        elif job_type == SizesWorker.get_job_type():
+        if job_type == SizesWorker.get_job_type():
             return SizesWorker(job_info=job_info, app_config=self.app_config)
-        else:
-            supported_job_types = [
-                ConfigNamesWorker.get_job_type(),
-                SplitsWorker.get_job_type(),
-                FirstRowsWorker.get_job_type(),
-                ParquetAndDatasetInfoWorker.get_job_type(),
-                ParquetWorker.get_job_type(),
-                DatasetInfoWorker.get_job_type(),
-                SizesWorker.get_job_type(),
-            ]
-            raise ValueError(f"Unsupported job type: '{job_type}'. The supported job types are: {supported_job_types}")
+        supported_job_types = [
+            ConfigNamesWorker.get_job_type(),
+            SplitsWorker.get_job_type(),
+            FirstRowsWorker.get_job_type(),
+            ParquetAndDatasetInfoWorker.get_job_type(),
+            ParquetWorker.get_job_type(),
+            DatasetInfoWorker.get_job_type(),
+            SizesWorker.get_job_type(),
+        ]
+        raise ValueError(f"Unsupported job type: '{job_type}'. The supported job types are: {supported_job_types}")
