c56d9f3f0bead50e85a37d7dfe3860f3763d9c93
Test User <sylvain.lesage@huggingface.co>
2023-02-08 15:42:06 +0000

    feat: 🎸 use primitive parameters, add release, add tests

--- services/worker/src/worker/resources.py
+++ services/worker/src/worker/resources.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2022 The HuggingFace Authors.
 
+from typing import Optional
+
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Optional
