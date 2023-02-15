97b50bae77b139dfc811bbe4f1453f42e3f75bd3
Quentin Lhoest <lhoest.q@gmail.com>
2023-02-15 14:30:13 +0100

    add worker loop executor

--- services/worker/src/worker/main.py
+++ services/worker/src/worker/main.py
@@ -1,53 +1,46 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2022 The HuggingFace Authors.
+from mirakuru import OutputExecutor
+import time
+import logging
+import sys
+import tempfile
+import os
 
+from filelock import FileLock
 from libcommon.log import init_logging
-from libcommon.processing_graph import ProcessingGraph
-from libcommon.resources import CacheMongoResource, QueueMongoResource
-from libcommon.storage import init_assets_dir
-
+from worker import start_worker_loop
 from worker.config import AppConfig
-from worker.job_runner_factory import JobRunnerFactory
-from worker.loop import Loop
-from worker.resources import LibrariesResource
+
+BANNER = "worker_banner"
 
 if __name__ == "__main__":
     app_config = AppConfig.from_env()
-
     init_logging(log_level=app_config.common.log_level)
-    # ^ set first to have logs as soon as possible
-    assets_directory = init_assets_dir(directory=app_config.assets.storage_directory)
+    
+    
+    with tempfile.TemporaryDirectory() as tmp_dir:
 
-    processing_graph = ProcessingGraph(app_config.processing_graph.specification)
+        worker_state_path = os.path.join(tmp_dir, 'worker_state.json')
 
-    with (
-        LibrariesResource(
-            hf_endpoint=app_config.common.hf_endpoint,
-            init_hf_datasets_cache=app_config.datasets_based.hf_datasets_cache,
-            numba_path=app_config.numba.path,
-        ) as libraries_resource,
-        CacheMongoResource(
-            database=app_config.cache.mongo_database, host=app_config.cache.mongo_url
-        ) as cache_resource,
-        QueueMongoResource(
-            database=app_config.queue.mongo_database, host=app_config.queue.mongo_url
-        ) as queue_resource,
-    ):
-        if not cache_resource.is_available():
-            raise RuntimeError("The connection to the cache database could not be established. Exiting.")
-        if not queue_resource.is_available():
-            raise RuntimeError("The connection to the queue database could not be established. Exiting.")
-
-        job_runner_factory = JobRunnerFactory(
-            app_config=app_config,
-            processing_graph=processing_graph,
-            hf_datasets_cache=libraries_resource.hf_datasets_cache,
-            assets_directory=assets_directory,
+        process = OutputExecutor(
+            [
+                sys.executable,
+                start_worker_loop.__file__,
+                "--banner",
+                BANNER,
+                "--worker_state_path",
+                worker_state_path
+            ],
+            banner=BANNER,
+            timeout=10
         )
-        loop = Loop(
-            library_cache_paths=libraries_resource.storage_paths,
-            job_runner_factory=job_runner_factory,
-            max_jobs_per_namespace=app_config.queue.max_jobs_per_namespace,
-            worker_config=app_config.worker,
-        )
-        loop.run()
+        process.start()
+        logging.warning("Worker process started.")
+        
+        for _ in range(5):
+            assert process.running()
+            with FileLock(worker_state_path + ".lock"):
+                with open(worker_state_path, 'r') as worker_state_f:
+                    logging.warning(f" OMG {worker_state_f.read()}")
+            time.sleep(1)
+
+        process.stop()
---
+++ services/worker/src/worker/start_worker_loop.py
@@ -0,0 +1,81 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 The HuggingFace Authors.
+
+import argparse
+import json
+
+from filelock import FileLock
+from libcommon.log import init_logging
+from libcommon.processing_graph import ProcessingGraph
+from libcommon.resources import CacheMongoResource, QueueMongoResource
+from libcommon.storage import init_assets_dir
+
+from worker.config import AppConfig
+from worker.job_runner_factory import JobRunnerFactory
+from worker.loop import Loop
+from worker.resources import LibrariesResource
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--banner')
+    parser.add_argument('--worker_state_path')
+    args = parser.parse_args()
+
+    if args.banner:
+        print(args.banner, flush=True)
+
+    app_config = AppConfig.from_env()
+
+    init_logging(log_level=app_config.common.log_level)
+
+    import logging
+    import time
+    
+
+    for i in range(60):
+        logging.warning(f"{i}")
+        if i % 2 == 0:
+            if args.worker_state_path:
+                with FileLock(args.worker_state_path + ".lock"):
+                    with open(args.worker_state_path, 'w') as worker_state_f:
+                        json.dump({"even_id": i}, worker_state_f)
+        time.sleep(1)
+    exit(0)
+    # ^ set first to have logs as soon as possible
+    assets_directory = init_assets_dir(directory=app_config.assets.storage_directory)
+
+    processing_graph = ProcessingGraph(app_config.processing_graph.specification)
+
+    with (
+        LibrariesResource(
+            hf_endpoint=app_config.common.hf_endpoint,
+            init_hf_datasets_cache=app_config.datasets_based.hf_datasets_cache,
+            numba_path=app_config.numba.path,
+        ) as libraries_resource,
+        CacheMongoResource(
+            database=app_config.cache.mongo_database, host=app_config.cache.mongo_url
+        ) as cache_resource,
+        QueueMongoResource(
+            database=app_config.queue.mongo_database, host=app_config.queue.mongo_url
+        ) as queue_resource,
+    ):
+        if not cache_resource.is_available():
+            raise RuntimeError("The connection to the cache database could not be established. Exiting.")
+        if not queue_resource.is_available():
+            raise RuntimeError("The connection to the queue database could not be established. Exiting.")
+
+        job_runner_factory = JobRunnerFactory(
+            app_config=app_config,
+            processing_graph=processing_graph,
+            hf_datasets_cache=libraries_resource.hf_datasets_cache,
+            assets_directory=assets_directory,
+        )
+        loop = Loop(
+            library_cache_paths=libraries_resource.storage_paths,
+            job_runner_factory=job_runner_factory,
+            max_jobs_per_namespace=app_config.queue.max_jobs_per_namespace,
+            worker_config=app_config.worker,
+        )
+        loop.run()
