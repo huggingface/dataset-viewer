6643f943619a6b8c2b4449c12eafc883db1873fe
Sylvain Lesage <sylvain.lesage@huggingface.co>
2023-03-10 11:13:15 +0100

    feat: 🎸 change log level to DEBUG (#917)
    also set the log level for requests to DEBUG. To investigate
    https://github.com/huggingface/datasets-server/issues/877. This commit
    should then be reverted.

--- libs/libcommon/src/libcommon/log.py
+++ libs/libcommon/src/libcommon/log.py
@@ -7,3 +7,7 @@ import logging
 def init_logging(log_level: int = logging.INFO) -> None:
     logging.basicConfig(level=log_level, format="%(levelname)s: %(asctime)s - %(name)s - %(message)s")
     logging.debug(f"Log level set to: {logging.getLevelName(logging.getLogger().getEffectiveLevel())}")
+
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(log_level)
+    requests_log.propagate = True
