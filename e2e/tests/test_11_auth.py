f9a8eb5ec9d162ddeffc3a96d742603f5e450685
Quentin Lhoest <lhoest.q@gmail.com>
2023-03-14 15:16:01 +0100

    sizes > size

--- e2e/tests/test_11_auth.py
+++ e2e/tests/test_11_auth.py
@@ -45,7 +45,7 @@ def test_auth_e2e(
         f"/parquet-and-dataset-info?dataset={dataset}",
         f"/parquet?dataset={dataset}",
         f"/dataset-info?dataset={dataset}",
-        f"/sizes?dataset={dataset}",
+        f"/size?dataset={dataset}",
     ]
     for endpoint in endpoints:
         poll_until_ready_and_assert(
