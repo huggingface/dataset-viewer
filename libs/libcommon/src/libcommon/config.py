29ad52c3d5edc5d23dbf967a6e0a4a6b8d7db11d
Andrea Soria <andrea@huggingface.co>
2023-03-15 07:52:59 -0400

    Fix rebase with new runners

--- libs/libcommon/src/libcommon/config.py
+++ libs/libcommon/src/libcommon/config.py
@@ -10,12 +10,13 @@ from environs import Env
 
 from libcommon.constants import (
     PROCESSING_STEP_CONFIG_NAMES_VERSION,
+    PROCESSING_STEP_CONFIG_PARQUET_VERSION,
     PROCESSING_STEP_CONFIG_SIZE_VERSION,
     PROCESSING_STEP_DATASET_INFO_VERSION,
+    PROCESSING_STEP_DATASET_PARQUET_VERSION,
     PROCESSING_STEP_DATASET_SIZE_VERSION,
     PROCESSING_STEP_FIRST_ROWS_VERSION,
     PROCESSING_STEP_PARQUET_AND_DATASET_INFO_VERSION,
-    PROCESSING_STEP_PARQUET_VERSION,
     PROCESSING_STEP_SPLIT_NAMES_FROM_DATASET_INFO_VERSION,
     PROCESSING_STEP_SPLIT_NAMES_FROM_STREAMING_VERSION,
     PROCESSING_STEP_SPLITS_VERSION,
@@ -129,10 +130,15 @@ class ProcessingGraphConfig:
                 "input_type": "dataset",
                 "job_runner_version": PROCESSING_STEP_PARQUET_AND_DATASET_INFO_VERSION,
             },
-            "/parquet": {
-                "input_type": "dataset",
+            "config-parquet": {
+                "input_type": "config",
                 "requires": "/parquet-and-dataset-info",
-                "job_runner_version": PROCESSING_STEP_PARQUET_VERSION,
+                "job_runner_version": PROCESSING_STEP_CONFIG_PARQUET_VERSION,
+            },
+            "dataset-parquet": {
+                "input_type": "dataset",
+                "requires": "config-parquet",
+                "job_runner_version": PROCESSING_STEP_DATASET_PARQUET_VERSION,
             },
             "/dataset-info": {
                 "input_type": "dataset",
