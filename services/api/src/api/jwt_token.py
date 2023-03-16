e455a510ead6d9c6bd355b4eee67b74c08d356cb
Test User <sylvain.lesage@huggingface.co>
2023-03-16 09:21:11 +0000

    feat: 🎸 add debug-level logs in the jwt bypass code

--- services/api/src/api/jwt_token.py
+++ services/api/src/api/jwt_token.py
@@ -4,6 +4,7 @@
 from typing import Any, Optional, Union
 
 import jwt
+import logging
 import requests
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric.ec import (
@@ -135,6 +136,10 @@ def is_jwt_valid(
         bool: True if the JWT is valid for the input dataset, else False
     """
     if not public_key or not algorithm:
+        logging.debug(
+            f"Missing public key '{public_key}' or algorithm '{algorithm}' to decode JWT token. Skipping JWT"
+            " validation."
+        )
         return False
     try:
         decoded = jwt.decode(
@@ -143,7 +148,12 @@ def is_jwt_valid(
             algorithms=[algorithm],
             options={"require": ["exp", "sub", "read"], "verify_exp": verify_exp},
         )
+        logging.debug(f"Decoded JWT is: '{public_key}'.")
     except Exception:
+        logging.debug(
+            f"Missing public key '{public_key}' or algorithm '{algorithm}' to decode JWT token. Skipping JWT"
+            " validation."
+        )
         return False
     sub = decoded.get("sub")
     if not isinstance(sub, str) or not sub.startswith("datasets/") or sub.removeprefix("datasets/") != dataset:
