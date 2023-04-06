b03aaf66bd573e4a0263cd0961d3e403f1f67b7a
Quentin Lhoest <lhoest.q@gmail.com>
2023-04-06 16:50:52 +0200

    support audio

--- services/api/src/api/routes/rows.py
+++ services/api/src/api/routes/rows.py
@@ -82,7 +82,7 @@ def get_parquet_fs(dataset: str, hf_token: Optional[str]) -> HfFileSystem:
     return HfFileSystem(dataset, repo_type="dataset", revision=PARQUET_REVISION, token=hf_token)
 
 
-UNSUPPORTED_FEATURES_MAGIC_STRINGS = ["Audio(", "'binary'"]
+UNSUPPORTED_FEATURES_MAGIC_STRINGS = ["'binary'"]
 
 
 class RowsIndex:
