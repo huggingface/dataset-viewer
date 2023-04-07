f6250932a78ffb1051d2bd61104bfe239d125489
Test User <sylvain.lesage@huggingface.co>
2023-04-07 07:41:17 +0000

    feat: 🎸 delete jobs and cache entries for /splits
    Note that for simplicity we delete all the "/splits" jobs, not only the
    pending ones. Anyway, the terminated jobs are deleted regularly.

---
+++ jobs/mongodb_migration/src/mongodb_migration/migrations/_20230407091400_queue_delete_splits.py
@@ -0,0 +1,27 @@
+import logging
+
+from mongoengine.connection import get_db
+
+from mongodb_migration.migration import IrreversibleMigrationError, Migration
+
+job_type = "/splits"
+db_name = "queue"
+
+
+# connection already occurred in the main.py (caveat: we use globals)
+class MigrationQueueDeleteSplits(Migration):
+    def up(self) -> None:
+        logging.info(f"Delete jobs of type {job_type}")
+
+        db = get_db(db_name)
+        db["jobsBlue"].delete_many({"type": job_type})
+
+    def down(self) -> None:
+        raise IrreversibleMigrationError("This migration does not support rollback")
+
+    def validate(self) -> None:
+        logging.info("Check that none of the documents has the /splits type")
+
+        db = get_db(db_name)
+        if db[db_name].count_documents({"type": job_type}):
+            raise ValueError(f"Found documents with type {job_type}")
