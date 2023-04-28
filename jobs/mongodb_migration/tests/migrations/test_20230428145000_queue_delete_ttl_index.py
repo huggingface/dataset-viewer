e0983833c3940ab839404c26974c8e405aacfef0
Sylvain Lesage <sylvain.lesage@huggingface.co>
2023-04-28 15:31:15 +0200

    feat: 🎸 change TTL for finished jobs from 7 days to 1 day (#1105)
    * feat: 🎸 change TTL for finished jobs from 7 days to 1 day
    
    We need to delete the index before mongoengine creates it again with a
    new value
    
    See https://github.com/huggingface/datasets-server/issues/1104
    
    * style: 💄 fix style

---
+++ jobs/mongodb_migration/tests/migrations/test_20230428145000_queue_delete_ttl_index.py
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2023 The HuggingFace Authors.
+
+from libcommon.constants import QUEUE_COLLECTION_JOBS, QUEUE_MONGOENGINE_ALIAS
+from libcommon.queue import Job
+from libcommon.resources import MongoResource
+from libcommon.utils import get_datetime
+from mongoengine.connection import get_db
+
+from mongodb_migration.migrations._20230428145000_queue_delete_ttl_index import (
+    MigrationQueueDeleteTTLIndexOnFinishedAt,
+    get_index_names,
+)
+
+
+def test_queue_delete_ttl_index(mongo_host: str) -> None:
+    with MongoResource(database="test_queue_delete_ttl_index", host=mongo_host, mongoengine_alias="queue"):
+        Job(type="test", dataset="test", unicity_id="test", namespace="test", created_at=get_datetime()).save()
+        db = get_db(QUEUE_MONGOENGINE_ALIAS)
+        assert (
+            len(get_index_names(db[QUEUE_COLLECTION_JOBS].index_information(), "finished_at")) == 1
+        )  # Ensure the TTL index exists
+
+        migration = MigrationQueueDeleteTTLIndexOnFinishedAt(
+            version="20230428145000",
+            description="remove ttl index on field 'finished_at'",
+        )
+        migration.up()
+
+        assert (
+            len(get_index_names(db[QUEUE_COLLECTION_JOBS].index_information(), "finished_at")) == 0
+        )  # Ensure the TTL index exists  # Ensure 0 records with old type
+
+        db[QUEUE_COLLECTION_JOBS].drop()
