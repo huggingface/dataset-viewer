476e58a1655856e335aead1dd1826778560f52cf
Test User <sylvain.lesage@huggingface.co>
2023-05-04 08:22:49 +0000

    fix: 🐛 fix glitches from rebase

--- services/worker/tests/job_runners/dataset/test_opt_in_out_urls_count.py
+++ services/worker/tests/job_runners/dataset/test_opt_in_out_urls_count.py
@@ -5,7 +5,7 @@ from http import HTTPStatus
 from typing import Any, Callable, List
 
 import pytest
-from libcommon.processing_graph import ProcessingStep
+from libcommon.processing_graph import ProcessingGraph
 from libcommon.queue import Priority
 from libcommon.resources import CacheMongoResource, QueueMongoResource
 from libcommon.simple_cache import upsert_response
@@ -36,6 +36,15 @@ def get_job_runner(
         app_config: AppConfig,
         force: bool = False,
     ) -> DatasetOptInOutUrlsCountJobRunner:
+        step_name = DatasetOptInOutUrlsCountJobRunner.get_job_type()
+        processing_graph = ProcessingGraph(
+            {
+                step_name: {
+                    "input_type": "dataset",
+                    "job_runner_version": DatasetOptInOutUrlsCountJobRunner.get_job_runner_version(),
+                }
+            }
+        )
         return DatasetOptInOutUrlsCountJobRunner(
             job_info={
                 "type": DatasetOptInOutUrlsCountJobRunner.get_job_type(),
@@ -48,16 +57,8 @@ def get_job_runner(
             },
             common_config=app_config.common,
             worker_config=app_config.worker,
-            processing_step=ProcessingStep(
-                name=DatasetOptInOutUrlsCountJobRunner.get_job_type(),
-                input_type="config",
-                requires=[],
-                required_by_dataset_viewer=False,
-                ancestors=[],
-                children=[],
-                parents=[],
-                job_runner_version=DatasetOptInOutUrlsCountJobRunner.get_job_runner_version(),
-            ),
+            processing_step=processing_graph.get_step(step_name),
+            processing_graph=processing_graph,
         )
 
     return _get_job_runner
