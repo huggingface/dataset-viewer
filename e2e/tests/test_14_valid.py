3b885783264cd29045decb486644116d2ba39ff1
Test User <sylvain.lesage@huggingface.co>
2023-05-10 11:14:56 +0000

    test: 💍 add logs

--- e2e/tests/test_14_valid.py
+++ e2e/tests/test_14_valid.py
@@ -2,7 +2,7 @@
 # Copyright 2022 The HuggingFace Authors.
 
 from .fixtures.hub import DatasetRepos
-from .utils import get
+from .utils import get, log
 
 
 def test_valid_after_datasets_processed(hf_dataset_repos_csv_data: DatasetRepos) -> None:
@@ -11,6 +11,8 @@ def test_valid_after_datasets_processed(hf_dataset_repos_csv_data: DatasetRepos)
     assert response.status_code == 200, f"{response.status_code} - {response.text}"
     # at this moment various datasets have been processed (due to the alphabetic order of the test files)
     valid = response.json()["valid"]
-    assert hf_dataset_repos_csv_data["public"] in valid, response.text
-    assert hf_dataset_repos_csv_data["gated"] in valid, response.text
-    assert hf_dataset_repos_csv_data["private"] not in valid, response.text
+    assert hf_dataset_repos_csv_data["public"] in valid, log(response, dataset=hf_dataset_repos_csv_data["public"])
+    assert hf_dataset_repos_csv_data["gated"] in valid, log(response, dataset=hf_dataset_repos_csv_data["gated"])
+    assert hf_dataset_repos_csv_data["private"] not in valid, log(
+        response, dataset=hf_dataset_repos_csv_data["private"]
+    )
