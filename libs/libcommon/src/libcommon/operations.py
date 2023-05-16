0ecd829f9caf230b9c712d4621ad338beeb017b4
Test User <sylvain.lesage@huggingface.co>
2023-05-16 08:08:34 +0000

    refactor: 💡 remove dead code

--- libs/libcommon/src/libcommon/operations.py
+++ libs/libcommon/src/libcommon/operations.py
@@ -11,44 +11,6 @@ from libcommon.state import DatasetState
 from libcommon.utils import Priority
 
 
-def update_dataset(
-    dataset: str,
-    processing_graph: ProcessingGraph,
-    hf_endpoint: str,
-    hf_token: Optional[str] = None,
-    priority: Priority = Priority.NORMAL,
-    hf_timeout_seconds: Optional[float] = None,
-) -> None:
-    """
-    Update a dataset
-
-    Args:
-        dataset (str): the dataset
-        processing_graph (ProcessingGraph): the processing graph
-        hf_endpoint (str): the HF endpoint
-        hf_token (Optional[str], optional): The HF token. Defaults to None.
-        priority (Priority, optional): The priority of the job. Defaults to Priority.NORMAL.
-        hf_timeout_seconds (Optional[float], optional): The timeout for requests to the hub. None means no timeout.
-          Defaults to None.
-
-    Returns: None.
-
-    Raises:
-        - [`~exceptions.AskAccessHubRequestError`]: if the request to the Hub to get access to the
-            dataset failed or timed out.
-        - [`~exceptions.DatasetInfoHubRequestError`]: if the request to the Hub to get the dataset
-            info failed or timed out.
-        - [`~exceptions.DatasetError`]: if the dataset could not be accessed or is not supported
-        - [`~exceptions.DatasetRevisionEmptyError`]
-          if the current git revision (branch, commit) could not be obtained.
-    """
-    revision = get_dataset_git_revision(
-        dataset=dataset, hf_endpoint=hf_endpoint, hf_token=hf_token, hf_timeout_seconds=hf_timeout_seconds
-    )
-    logging.debug(f"refresh dataset='{dataset}'")
-    backfill_dataset(dataset=dataset, processing_graph=processing_graph, revision=revision, priority=priority)
-
-
 def backfill_dataset(
     dataset: str,
     revision: str,
