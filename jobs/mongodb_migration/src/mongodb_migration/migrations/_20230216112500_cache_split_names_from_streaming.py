e598da98c0e86ed6a5dcda5cdfc30b17e0defa14
polinaeterna <polina@huggingface.co>
2023-05-17 20:15:59 +0200

    refactor old renaming migrations

--- jobs/mongodb_migration/src/mongodb_migration/migrations/_20230216112500_cache_split_names_from_streaming.py
+++ jobs/mongodb_migration/src/mongodb_migration/migrations/_20230216112500_cache_split_names_from_streaming.py
@@ -1,44 +1,23 @@
 # SPDX-License-Identifier: Apache-2.0
-# Copyright 2022 The HuggingFace Authors.
+# Copyright 2023 The HuggingFace Authors.
 
 import logging
 
 from libcommon.constants import CACHE_COLLECTION_RESPONSES, CACHE_MONGOENGINE_ALIAS
-from libcommon.simple_cache import CachedResponse
 from mongoengine.connection import get_db
 
-from mongodb_migration.check import check_documents
-from mongodb_migration.migration import Migration
-
-split_names = "/split-names"
-split_names_from_streaming = "/split-names-from-streaming"
-split_names_tmp = "/split-names-TMP"
+from mongodb_migration.renaming_migrations import CacheRenamingMigration
 
 
 # connection already occurred in the main.py (caveat: we use globals)
-class MigrationCacheUpdateSplitNames(Migration):
+class MigrationCacheUpdateSplitNames(CacheRenamingMigration):
     def up(self) -> None:
-        logging.info(f"Rename cache_kind field from {split_names} to {split_names_from_streaming}")
+        tmp_kind = f"{self.cache_kind}-TMP"
+        logging.info(f"Rename cache_kind field from {self.cache_kind} to {self.new_cache_kind}")
         db = get_db(CACHE_MONGOENGINE_ALIAS)
         # update existing documents with the new kind (if any) to avoid duplicates (will be deleted later)
-        db[CACHE_COLLECTION_RESPONSES].update_many(
-            {"kind": split_names_from_streaming}, {"$set": {"kind": split_names_tmp}}
-        )
+        db[CACHE_COLLECTION_RESPONSES].update_many({"kind": self.new_cache_kind}, {"$set": {"kind": tmp_kind}})
         # update existing documents with the old kind
-        db[CACHE_COLLECTION_RESPONSES].update_many(
-            {"kind": split_names}, {"$set": {"kind": split_names_from_streaming}}
-        )
+        db[CACHE_COLLECTION_RESPONSES].update_many({"kind": self.cache_kind}, {"$set": {"kind": self.new_cache_kind}})
         # delete the duplicates
-        db[CACHE_COLLECTION_RESPONSES].delete_many({"kind": split_names_tmp})
-
-    def down(self) -> None:
-        logging.info(f"Rollback cache_kind field from {split_names_from_streaming} to {split_names}")
-        db = get_db(CACHE_MONGOENGINE_ALIAS)
-        db[CACHE_COLLECTION_RESPONSES].update_many(
-            {"kind": split_names_from_streaming}, {"$set": {"kind": split_names}}
-        )
-
-    def validate(self) -> None:
-        logging.info("Validate modified documents")
-
-        check_documents(DocCls=CachedResponse, sample_size=10)
+        db[CACHE_COLLECTION_RESPONSES].delete_many({"kind": tmp_kind})
