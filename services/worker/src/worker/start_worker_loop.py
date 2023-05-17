0cb93b036a0d5adc4312f4971a923ccdade988df
Quentin Lhoest <lhoest.q@gmail.com>
2023-05-17 18:45:01 +0200

    missing arg

--- services/worker/src/worker/start_worker_loop.py
+++ services/worker/src/worker/start_worker_loop.py
@@ -6,7 +6,7 @@ import sys
 from libcommon.log import init_logging
 from libcommon.processing_graph import ProcessingGraph
 from libcommon.resources import CacheMongoResource, QueueMongoResource
-from libcommon.storage import init_assets_dir
+from libcommon.storage import init_assets_dir, init_parquet_metadata_dir
 
 from worker.config import AppConfig
 from worker.job_runner_factory import JobRunnerFactory
@@ -25,6 +25,7 @@ if __name__ == "__main__":
     init_logging(level=app_config.log.level)
     # ^ set first to have logs as soon as possible
     assets_directory = init_assets_dir(directory=app_config.assets.storage_directory)
+    parquet_metadata_directory = init_parquet_metadata_dir(directory=app_config.parquet_metadata.storage_directory)
 
     processing_graph = ProcessingGraph(app_config.processing_graph.specification)
 
@@ -51,6 +52,7 @@ if __name__ == "__main__":
             processing_graph=processing_graph,
             hf_datasets_cache=libraries_resource.hf_datasets_cache,
             assets_directory=assets_directory,
+            parquet_metadata_directory=parquet_metadata_directory,
         )
         loop = Loop(
             library_cache_paths=libraries_resource.storage_paths,
