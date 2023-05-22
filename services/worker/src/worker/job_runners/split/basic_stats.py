4b3643e2642f23f6d602c196f6d4fbace6a97882
polinaeterna <polina@huggingface.co>
2023-05-22 15:45:49 +0200

    update statistics

--- services/worker/src/worker/job_runners/split/basic_stats.py
+++ services/worker/src/worker/job_runners/split/basic_stats.py
@@ -2,17 +2,17 @@
 # Copyright 2023 The HuggingFace Authors.
 
 import logging
-from http import HTTPStatus
 from pathlib import Path
-from typing import List, Literal, Optional, TypedDict, Union
+from typing import List, Literal, TypedDict
 
+import dask.array as da
 import dask.dataframe as dd
 from libcommon.constants import PROCESSING_STEP_SPLIT_BASIC_STATS_VERSION
+from libcommon.exceptions import PreviousStepFormatError
 from libcommon.processing_graph import ProcessingStep
 from libcommon.utils import JobInfo
 from pandas.api.types import is_numeric_dtype
 
-from worker.common_exceptions import JobRunnerError
 from worker.config import AppConfig
 from worker.job_runners.split.split_job_runner import SplitCachedJobRunner
 from worker.utils import CompleteJobResult, get_previous_step_or_raise
@@ -20,50 +20,46 @@ from worker.utils import CompleteJobResult, get_previous_step_or_raise
 SplitBasicStatsJobRunnerErrorCode = Literal["PreviousStepFormatError"]
 
 
-class SplitBasicStatsJobRunnerError(JobRunnerError):
-    """Base class for exceptions in this module."""
-
-    def __init__(
-        self,
-        message: str,
-        status_code: HTTPStatus,
-        code: SplitBasicStatsJobRunnerErrorCode,
-        cause: Optional[BaseException] = None,
-        disclose_cause: bool = False,
-    ):
-        super().__init__(
-            message=message, status_code=status_code, code=code, cause=cause, disclose_cause=disclose_cause
-        )
+class Histogram(TypedDict):
+    hist: List[int]
+    bin_edges: List[float]
 
 
-class PreviousStepFormatError(SplitBasicStatsJobRunnerError):
-    """Raised when the content of the previous step has not the expected format."""
-
-    def __init__(self, message: str, cause: Optional[BaseException] = None):
-        super().__init__(message, HTTPStatus.INTERNAL_SERVER_ERROR, "PreviousStepFormatError", cause, False)
-
-
-class BasicStatsItem(TypedDict):
-    min: Union[int, float]
-    max: Union[int, float]
-    median: Union[int, float]
-    # TODO
+class ContinuousStatsItem(TypedDict):
+    min: float
+    max: float
+    mean: float
+    median: float
+    std: float
+    histogram: Histogram
 
 
-class BasicStatsPerColumnItem(TypedDict):
+class ContinuousStatsPerColumnItem(TypedDict):
     column_name: str
-    column_stats: BasicStatsItem
+    # column_type: str
+    column_stats: ContinuousStatsItem
 
 
 class SplitBasicStatsResponse(TypedDict):
-    basic_stats: List[BasicStatsPerColumnItem]
+    basic_stats: List[ContinuousStatsPerColumnItem]
 
 
-# def compute_histogram(series = dd.Series) -> Histogram:
-#     histogram_range = series.min(), series.max()
-#     hist, bin_edges = dd.compute(*da.histogram(series.to_dask_array(), bins=10, range=histogram_range))
-#     bin_edges = bin_edges.round(decimals=14)  # round up numpy precision issues
-#     return Histogram(hist=hist.tolist(), bin_edges=bin_edges.tolist())
+def compute_continuous_stats(series: dd.Series) -> ContinuousStatsItem:
+    histogram_range = series.min(), series.max()
+    hist, bin_edges = dd.compute(*da.histogram(series.to_dask_array(), bins=10, range=histogram_range))
+    bin_edges = bin_edges.round(decimals=14)  # round up numpy precision issues
+    bin_edges = bin_edges.tolist()
+    minimum, maximum = bin_edges[0], bin_edges[-1]
+    mean, median, std = series.mean().compute().item(), series.median().compute().item(), series.std().compute().item()
+    histogram = Histogram(hist=hist.to_list(), bin_edges=bin_edges)
+    return ContinuousStatsItem(
+        min=minimum,
+        max=maximum,
+        mean=mean,
+        median=median,
+        std=std,
+        histogram=histogram,
+    )
 
 
 def compute_basic_stats_response(
@@ -83,7 +79,7 @@ def compute_basic_stats_response(
     if "parquet_files" not in content:
         raise PreviousStepFormatError(f"previous step '{previous_step} doesn't return expected field: 'parquet_files'")
 
-    basic_stats: List[BasicStatsPerColumnItem] = []
+    basic_stats: List[ContinuousStatsPerColumnItem] = []
     parquet_files_urls = [
         parquet_file["url"]
         for parquet_file in content["parquet_files"]
@@ -92,15 +88,8 @@ def compute_basic_stats_response(
     df = dd.read_parquet(parquet_files_urls)
     for column_name in df:
         if is_numeric_dtype(df[column_name].dtype):
-            # this is silly but it's just to try
-            minimum = df[column_name].min().compute().item()
-            maximum = df[column_name].max().compute().item()
-            median = df[column_name].median().compute().item()
-            basic_stats.append(
-                BasicStatsPerColumnItem(
-                    column_name=column_name, column_stats=BasicStatsItem(min=minimum, max=maximum, median=median)
-                )
-            )
+            column_stats = compute_continuous_stats(df[column_name])
+            basic_stats.append(ContinuousStatsPerColumnItem(column_name=column_name, column_stats=column_stats))
 
     return SplitBasicStatsResponse(basic_stats=basic_stats)
 
