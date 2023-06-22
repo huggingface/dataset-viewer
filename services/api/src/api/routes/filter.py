667ef4c890a3d0239ab8b1132fbc3f82e83d45ad
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-06-22 09:30:18 +0200

    Check authentication

--- services/api/src/api/routes/filter.py
+++ services/api/src/api/routes/filter.py
@@ -8,6 +8,7 @@ from libcommon.prometheus import StepProfiler
 from starlette.requests import Request
 from starlette.responses import Response
 
+from api.authentication import auth_check
 from api.utils import (
     ApiCustomError,
     Endpoint,
@@ -27,6 +28,10 @@ logger = logging.getLogger(__name__)
 
 
 def create_filter_endpoint(
+    hf_jwt_public_key: Optional[str] = None,
+    hf_jwt_algorithm: Optional[str] = None,
+    external_auth_url: Optional[str] = None,
+    hf_timeout_seconds: Optional[float] = None,
     max_age_long: int = 0,
     max_age_short: int = 0,
 ) -> Endpoint:
@@ -53,6 +58,16 @@ def create_filter_endpoint(
                     logger.info(
                         f'/filter, dataset={dataset}, config={config}, split={split}, where="{where}",'
                         f" offset={offset}, length={length}"
+                    )
+                with StepProfiler(method="filter_endpoint", step="check authentication"):
+                    # If auth_check fails, it will raise an exception that will be caught below
+                    auth_check(
+                        dataset=dataset,
+                        external_auth_url=external_auth_url,
+                        request=request,
+                        hf_jwt_public_key=hf_jwt_public_key,
+                        hf_jwt_algorithm=hf_jwt_algorithm,
+                        hf_timeout_seconds=hf_timeout_seconds,
                     )
                 with StepProfiler(method="filter_endpoint", step="create response"):
                     response = {"status": "ok"}
