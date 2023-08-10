261d7be2e28cdd636b64790948cc9bf94327bf78
Andrea Soria <andrea@huggingface.co>
2023-08-10 18:28:56 -0400

    Log metrics difference

--- jobs/cache_maintenance/src/cache_maintenance/cache_metrics.py
+++ jobs/cache_maintenance/src/cache_maintenance/cache_metrics.py
@@ -12,9 +12,18 @@ from libcommon.simple_cache import (
 def collect_cache_metrics() -> None:
     logging.info("collecting cache metrics")
     for metric in get_responses_count_by_kind_status_and_error_code():
-        # TODO: Get difference and store it somewhere?
-        # TODO: Maybe initially just log the difference
-        CacheTotalMetricDocument.objects(
-            kind=metric["kind"], http_status=metric["http_status"], error_code=metric["error_code"]
-        ).upsert_one(total=metric["count"])
+        kind = metric["kind"]
+        http_status = metric["http_status"]
+        error_code = metric["error_code"]
+        current_total = metric["count"]
+
+        query_set = CacheTotalMetricDocument.objects(kind=kind, http_status=http_status, error_code=error_code)
+        previous_metric = query_set.first()
+        if previous_metric is not None:
+            previous_total = previous_metric.total
+            logging.info(
+                f"{kind=} {http_status=} {error_code=}  previous_total={previous_total} current_total="
+                f"{current_total} difference={current_total-previous_total}"
+            )
+        query_set.upsert_one(total=metric["count"])
     logging.info("metrics have been collected")
