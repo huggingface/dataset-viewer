145e1a2b9a668fdf495131921b96cb31dae35b39
Andrea Soria <andrea@huggingface.co>
2023-08-11 06:53:43 -0400

    Adding migration to drop collection

--- jobs/mongodb_migration/src/mongodb_migration/deletion_migrations.py
+++ jobs/mongodb_migration/src/mongodb_migration/deletion_migrations.py
@@ -4,7 +4,6 @@
 import logging
 from typing import Any, List, Mapping, Optional
 
-from libcommon.constants import CACHE_METRICS_COLLECTION, METRICS_MONGOENGINE_ALIAS
 from mongoengine.connection import get_db
 
 from mongodb_migration.migration import (
@@ -12,7 +11,6 @@ from mongodb_migration.migration import (
     CacheMigration,
     IrreversibleMigrationError,
     MetricsMigration,
-    Migration,
     QueueMigration,
 )
 
@@ -162,22 +160,3 @@ class CacheMetricsDeletionMigration(MetricsMigration):
             raise ValueError(f"Found documents with type {self.job_type}")
         if db[self.COLLECTION_CACHE_TOTAL_METRIC].count_documents({"kind": self.cache_kind}):
             raise ValueError(f"Found documents with kind {self.cache_kind}")
-
-
-class CacheMetricsCollectionDeletionMigration(Migration):
-    def up(self) -> None:
-        logging.info(f"Delete {CACHE_METRICS_COLLECTION} collection from {METRICS_MONGOENGINE_ALIAS}")
-
-        db = get_db(METRICS_MONGOENGINE_ALIAS)
-        db[CACHE_METRICS_COLLECTION].drop()
-
-    def down(self) -> None:
-        raise IrreversibleMigrationError("This migration does not support rollback")
-
-    def validate(self) -> None:
-        logging.info("Check that collection does not exist")
-
-        db = get_db(METRICS_MONGOENGINE_ALIAS)
-        collections = db.list_collection_names()
-        if CACHE_METRICS_COLLECTION in collections:
-            raise ValueError(f"Found collection with name {CACHE_METRICS_COLLECTION}")
