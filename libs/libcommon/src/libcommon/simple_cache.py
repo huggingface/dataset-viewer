a11838db333829410c4673738e306ec059f73d5d
Andrea Soria <andrea@huggingface.co>
2023-08-17 15:28:06 -0400

    Temporarily delete index

--- libs/libcommon/src/libcommon/simple_cache.py
+++ libs/libcommon/src/libcommon/simple_cache.py
@@ -144,7 +144,6 @@ class CacheTotalMetricDocument(Document):
     meta = {
         "collection": CACHE_METRICS_COLLECTION,
         "db_alias": CACHE_MONGOENGINE_ALIAS,
-        "indexes": [("kind", "http_status", "error_code")],
     }
     objects = QuerySetManager["CacheTotalMetricDocument"]()
 
