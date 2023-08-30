d6c2d214a79b4655aeb1d52e7b3d6fe87962e58f
Quentin Lhoest <lhoest.q@gmail.com>
2023-08-30 15:11:02 +0200

    enable audio in search

--- services/search/src/search/routes/search.py
+++ services/search/src/search/routes/search.py
@@ -53,7 +53,7 @@ logger = logging.getLogger(__name__)
 
 ROW_IDX_COLUMN = "__hf_index_id"
 MAX_ROWS = 100
-UNSUPPORTED_FEATURES = [Value("binary"), Audio()]
+UNSUPPORTED_FEATURES = [Value("binary")]
 
 FTS_COMMAND_COUNT = (
     "SELECT COUNT(*) FROM (SELECT __hf_index_id, fts_main_data.match_bm25(__hf_index_id, ?) AS __hf_fts_score FROM"
