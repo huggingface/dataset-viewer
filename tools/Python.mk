fed4257b88b0491af32a60889b1cb83d6c10e9f2
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2023-08-31 12:03:39 +0200

    ignore gitpython vuln (#1744)

--- tools/Python.mk
+++ tools/Python.mk
@@ -28,5 +28,5 @@ style:
 
 .PHONY: pip-audit
 pip-audit:
-	bash -c "poetry run pip-audit -r <(poetry export -f requirements.txt --with dev  | sed '/^kenlm @/d' |sed '/^torch @/d' | sed '/^libapi @/d' | sed '/^libcommon @/d' | sed '/^trec-car-tools @/d')"
+	bash -c "poetry run pip-audit --ignore-vuln GHSA-wfm5-v35h-vwf4 -r <(poetry export -f requirements.txt --with dev  | sed '/^kenlm @/d' |sed '/^torch @/d' | sed '/^libapi @/d' | sed '/^libcommon @/d' | sed '/^trec-car-tools @/d')"
 # ^ we remove problematic lines to have a working pip-audit. See https://github.com/pypa/pip-audit/issues/84#issuecomment-1326203111 for "requests"
