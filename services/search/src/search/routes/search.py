7c368eaaccf6e29a7216a90fc7ba1b9926e5adb3
Andrea Soria <andrea@huggingface.co>
2023-09-07 12:28:39 -0400

    Change sort column in search

--- services/search/src/search/routes/search.py
+++ services/search/src/search/routes/search.py
@@ -57,7 +57,7 @@ FTS_COMMAND_COUNT = (
 
 FTS_COMMAND = (
     "SELECT * EXCLUDE (__hf_fts_score) FROM (SELECT *, fts_main_data.match_bm25(__hf_index_id, ?) AS __hf_fts_score"
-    " FROM data) A WHERE __hf_fts_score IS NOT NULL ORDER BY __hf_index_id OFFSET {offset} LIMIT {length};"
+    " FROM data) A WHERE __hf_fts_score IS NOT NULL ORDER BY __hf_fts_score DESC OFFSET {offset} LIMIT {length};"
 )
 REPO_TYPE = "dataset"
 HUB_DOWNLOAD_CACHE_FOLDER = "cache"
