b92eeb52fe946fa08448e27988180e4810420201
Test User <sylvain.lesage@huggingface.co>
2023-09-11 23:27:39 +0000

    feat: 🎸 set replicaset on the local mongodb container
    it's needed to "watch" the changes; required for the sse service

--- tools/Docker.mk
+++ tools/Docker.mk
@@ -5,3 +5,4 @@ down:
 .PHONY: up
 up:	
 	docker compose -f $(DOCKER_COMPOSE) up -d --build --force-recreate --remove-orphans --renew-anon-volumes
+	echo "rs.initiate({_id:'rs0',version:1,members:[{_id:0,host:'localhost:$(MONGO_PORT)'}]})" | mongosh --port $(MONGO_PORT) --quiet
