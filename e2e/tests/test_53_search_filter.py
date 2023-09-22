a72d5ddf03e5886a04cdce30deb97ba4358d4130
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-09-22 11:34:36 +0200

    Add e2e test

--- e2e/tests/test_53_search_filter.py
+++ e2e/tests/test_53_search_filter.py
@@ -1,11 +1,8 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2023 The HuggingFace Authors.
-
 from .fixtures.hub import AuthHeaders, AuthType
 from .utils import get_default_config_split, poll_until_ready_and_assert
 
 
-def test_search_endpoint(
+def test_filter_endpoint(
     auth_headers: AuthHeaders,
     hf_public_dataset_repo_csv_data: str,
 ) -> None:
@@ -16,14 +13,11 @@ def test_search_endpoint(
     dataset = hf_public_dataset_repo_csv_data
     config, split = get_default_config_split()
     headers = auth_headers[auth]
-    # ensure the /search endpoint works as well
     offset = 1
     length = 2
-    query = "Lord Vader"
+    where = "col_4 = 'B'"
     search_response = poll_until_ready_and_assert(
-        relative_url=(
-            f"/search?dataset={dataset}&config={config}&split={split}&offset={offset}&length={length}&query={query}"
-        ),
+        relative_url=f"/filter?{dataset=}&{config=}&{split=}&{offset=}&{length=}&{where=}",
         expected_status_code=expected_status_code,
         expected_error_code=expected_error_code,
         headers=headers,
@@ -45,7 +39,7 @@ def test_search_endpoint(
         assert num_rows_per_page == 100
         assert rows[0] == {
             "row_idx": 2,
-            "row": {"col_1": "We count thirty Rebel ships, Lord Vader.", "col_2": 2, "col_3": 2.0, "col_4": "A"},
+            "row": {"col_1": "We count thirty Rebel ships, Lord Vader.", "col_2": 2, "col_3": 2.0},
             "truncated_cells": [],
         }, rows[0]
         assert rows[1] == {
@@ -54,7 +48,6 @@ def test_search_endpoint(
                 "col_1": "The wingman spots the pirateship coming at him and warns the Dark Lord",
                 "col_2": 3,
                 "col_3": 3.0,
-                "col_4": "B",
             },
             "truncated_cells": [],
         }, rows[1]
