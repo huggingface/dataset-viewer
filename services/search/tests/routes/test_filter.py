52f9fe161e7f43f0eb31a112169803ca8b103a21
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-09-28 09:40:24 +0200

    Uncomment filter test_create_response

--- services/search/tests/routes/test_filter.py
+++ services/search/tests/routes/test_filter.py
@@ -8,7 +8,11 @@ import duckdb
 import pyarrow as pa
 import pytest
 from datasets import Dataset
+from libapi.response import create_response
+from libcommon.s3_client import S3Client
+from libcommon.storage import StrPath
 
+from search.config import AppConfig
 from search.routes.filter import execute_filter_query
 
 
@@ -51,44 +55,49 @@ def test_execute_filter_query(index_file_location: str) -> None:
     assert pa_table == pa.Table.from_pydict({"name": ["Simone"], "age": [30]})
 
 
-# def test_create_response(ds: Dataset, app_config: AppConfig, cached_assets_directory: StrPath) -> None:
-#     dataset, config, split = "ds", "default", "train"
-#     offset = 2
-#     pa_table = pa.Table.from_pydict(
-#         {
-#             "name": ["Marie", "Paul", "Leo", "Simone"],
-#             "gender": ["female", "male", "male", "female"],
-#             "age": [35, 30, 25, 30],
-#         }
-#     )
-#     response = create_response(
-#         dataset=dataset,
-#         config=config,
-#         split=split,
-#         cached_assets_base_url=app_config.cached_assets.base_url,
-#         cached_assets_directory=cached_assets_directory,
-#         # TODO:
-#         # s3_client=s3_client,
-#         # cached_assets_s3_bucket=cached_assets_s3_bucket,
-#         # cached_assets_s3_folder_name=cached_assets_s3_folder_name,
-#         pa_table=pa_table,
-#         offset=offset,
-#         features=ds.features,
-#         unsupported_columns=[],
-#         num_rows_total=4,
-#     )
-#     assert response == {
-#         "features": [
-#             {"feature_idx": 0, "name": "name", "type": {"dtype": "string", "_type": "Value"}},
-#             {"feature_idx": 1, "name": "gender", "type": {"dtype": "string", "_type": "Value"}},
-#             {"feature_idx": 2, "name": "age", "type": {"dtype": "int64", "_type": "Value"}},
-#         ],
-#         "rows": [
-#             {"row_idx": 2, "row": {"name": "Marie", "gender": "female", "age": 35}, "truncated_cells": []},
-#             {"row_idx": 3, "row": {"name": "Paul", "gender": "male", "age": 30}, "truncated_cells": []},
-#             {"row_idx": 4, "row": {"name": "Leo", "gender": "male", "age": 25}, "truncated_cells": []},
-#             {"row_idx": 5, "row": {"name": "Simone", "gender": "female", "age": 30}, "truncated_cells": []},
-#         ],
-#         "num_rows_total": 4,
-#         "num_rows_per_page": 100,
-#     }
+def test_create_response(ds: Dataset, app_config: AppConfig, cached_assets_directory: StrPath) -> None:
+    dataset, config, split = "ds", "default", "train"
+    offset = 2
+    pa_table = pa.Table.from_pydict(
+        {
+            "name": ["Marie", "Paul", "Leo", "Simone"],
+            "gender": ["female", "male", "male", "female"],
+            "age": [35, 30, 25, 30],
+        }
+    )
+    s3_client = S3Client(
+        region_name=app_config.cached_assets_s3.region,
+        aws_access_key_id=app_config.cached_assets_s3.access_key_id,
+        aws_secret_access_key=app_config.cached_assets_s3.secret_access_key,
+    )
+    response = create_response(
+        dataset=dataset,
+        config=config,
+        split=split,
+        cached_assets_base_url=app_config.cached_assets.base_url,
+        cached_assets_directory=cached_assets_directory,
+        # TODO:
+        s3_client=s3_client,
+        cached_assets_s3_bucket=app_config.cached_assets_s3.bucket,
+        cached_assets_s3_folder_name=app_config.cached_assets_s3.folder_name,
+        pa_table=pa_table,
+        offset=offset,
+        features=ds.features,
+        unsupported_columns=[],
+        num_rows_total=4,
+    )
+    assert response == {
+        "features": [
+            {"feature_idx": 0, "name": "name", "type": {"dtype": "string", "_type": "Value"}},
+            {"feature_idx": 1, "name": "gender", "type": {"dtype": "string", "_type": "Value"}},
+            {"feature_idx": 2, "name": "age", "type": {"dtype": "int64", "_type": "Value"}},
+        ],
+        "rows": [
+            {"row_idx": 2, "row": {"name": "Marie", "gender": "female", "age": 35}, "truncated_cells": []},
+            {"row_idx": 3, "row": {"name": "Paul", "gender": "male", "age": 30}, "truncated_cells": []},
+            {"row_idx": 4, "row": {"name": "Leo", "gender": "male", "age": 25}, "truncated_cells": []},
+            {"row_idx": 5, "row": {"name": "Simone", "gender": "female", "age": 30}, "truncated_cells": []},
+        ],
+        "num_rows_total": 4,
+        "num_rows_per_page": 100,
+    }
