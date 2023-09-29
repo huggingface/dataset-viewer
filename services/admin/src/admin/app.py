f2205134a985acb042c9bda5131a7b30b96fb315
Test User <sylvain.lesage@huggingface.co>
2023-09-29 09:39:54 +0000

    fix: 🐛 fix import

--- services/admin/src/admin/app.py
+++ services/admin/src/admin/app.py
@@ -2,7 +2,6 @@
 # Copyright 2022 The HuggingFace Authors.
 
 import uvicorn
-from admim.route.recreate_dataset import create_recreate_dataset_endpoint
 from libcommon.log import init_logging
 from libcommon.processing_graph import ProcessingGraph
 from libcommon.resources import CacheMongoResource, QueueMongoResource, Resource
@@ -41,6 +40,7 @@ from admin.routes.obsolete_cache import (
     create_get_obsolete_cache_endpoint,
 )
 from admin.routes.pending_jobs import create_pending_jobs_endpoint
+from admin.routes.recreate_dataset import create_recreate_dataset_endpoint
 from admin.utils import EXPOSED_HEADERS
 
 
