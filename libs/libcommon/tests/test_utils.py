a55d31b3c1be11a114772c4ba7d3a464dc93bfc7
Andrea Francis Soria Jimenez <andrea@huggingface.co>
2023-10-06 14:34:39 -0400

    fix: adding support for other data types in orjson_dumps (#1938)
    * adding support for other types
    
    * add options
    
    * add code review suggestion

--- libs/libcommon/tests/test_utils.py
+++ libs/libcommon/tests/test_utils.py
@@ -4,10 +4,17 @@
 from contextlib import nullcontext as does_not_raise
 from typing import Any
 
+import numpy as np
+import pandas as pd
 import pytest
 
 from libcommon.exceptions import DatasetInBlockListError
-from libcommon.utils import inputs_to_string, is_image_url, raise_if_blocked
+from libcommon.utils import (
+    inputs_to_string,
+    is_image_url,
+    orjson_dumps,
+    raise_if_blocked,
+)
 
 
 @pytest.mark.parametrize(
@@ -62,3 +69,19 @@ def test_is_image_url(text: str, expected: bool) -> None:
 def test_raise_if_blocked(dataset: str, blocked: list[str], expectation: Any) -> None:
     with expectation:
         raise_if_blocked(dataset=dataset, blocked_datasets=blocked)
+
+
+def test_orjson_dumps() -> None:
+    obj = {
+        "int": 1,
+        "bool": True,
+        "str": "text",
+        "np_array": np.array([1, 2, 3, 4]),
+        "np_int64": np.int64(12),
+        "pd_timedelta": pd.Timedelta(1, "d"),
+        "object": {"a": 1, "b": 10.2},
+        "non_string_key": {1: 20},
+        "pd_timestmap": pd.Timestamp("2023-10-06"),
+    }
+    serialized_obj = orjson_dumps(obj)
+    assert serialized_obj is not None
