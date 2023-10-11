bf425e766899881fc2e24b6a7ee871e09e9e9f74
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2023-10-11 17:02:06 +0200

    Fix clean hf datasets cache pattern (#1966)
    * fix clean hf datasets cache pattern
    
    * minor

--- jobs/cache_maintenance/tests/test_clean_hf_datasets_cache.py
+++ jobs/cache_maintenance/tests/test_clean_hf_datasets_cache.py
@@ -1,9 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2023 The HuggingFace Authors.
 
-import os
 import time
-from pathlib import Path
 
 from pytest import TempPathFactory
 
@@ -11,24 +9,24 @@ from cache_maintenance.clean_hf_datasets_cache import clean_hf_datasets_cache
 
 
 def test_clean_hf_datasets_cache(tmp_path_factory: TempPathFactory) -> None:
-    hf_datasets_cache_directory = str(tmp_path_factory.mktemp("test_clean_hf_datasets_cache") / "hf_datasets_cache")
+    hf_datasets_cache_directory = tmp_path_factory.mktemp("test_clean_hf_datasets_cache") / "hf_datasets_cache"
+    hf_datasets_cache_directory.mkdir(parents=True, exist_ok=True)
     expired_time_interval_seconds = 2
-    os.mkdir(hf_datasets_cache_directory)
 
-    cache_file = Path(f"{hf_datasets_cache_directory}/foo")
+    test_dataset_cache = hf_datasets_cache_directory / "medium/datasets/test_dataset_cache"
+    test_dataset_cache.mkdir(parents=True, exist_ok=True)
+    cache_file = test_dataset_cache / "foo"
     cache_file.touch()
 
     # ensure file exists
     assert cache_file.is_file()
 
     # try to delete it inmediatly after creation, it should remain
-    clean_hf_datasets_cache(hf_datasets_cache_directory, expired_time_interval_seconds)
+    clean_hf_datasets_cache(str(hf_datasets_cache_directory), expired_time_interval_seconds)
     assert cache_file.is_file()
 
     # try to delete it after more that time interval, it should be deleted
     cache_file.touch()
     time.sleep(expired_time_interval_seconds + 2)
-    clean_hf_datasets_cache(hf_datasets_cache_directory, expired_time_interval_seconds)
+    clean_hf_datasets_cache(str(hf_datasets_cache_directory), expired_time_interval_seconds)
     assert not cache_file.is_file()
-
-    os.rmdir(f"{hf_datasets_cache_directory}")
