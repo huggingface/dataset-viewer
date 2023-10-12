58c63965ad59286a8872cefab3151808acf3ac94
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2023-10-12 10:23:39 +0200

    Remove duplicate admin are_valid_parameters

--- services/admin/src/admin/utils.py
+++ services/admin/src/admin/utils.py
@@ -1,12 +1,2 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2022 The HuggingFace Authors.
-
-from typing import Any
-
-
-def is_non_empty_string(string: Any) -> bool:
-    return isinstance(string, str) and bool(string and string.strip())
-
-
-def are_valid_parameters(parameters: list[Any]) -> bool:
-    return all(is_non_empty_string(s) for s in parameters)
