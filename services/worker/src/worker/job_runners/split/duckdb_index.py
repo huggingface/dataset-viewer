d23d0869afdce9bd1c4ed3248ec336ae87f9964d
polinaeterna <polina@huggingface.co>
2023-10-13 18:01:09 +0200

    remove from duckdb-index worker too

--- services/worker/src/worker/job_runners/split/duckdb_index.py
+++ services/worker/src/worker/job_runners/split/duckdb_index.py
@@ -154,8 +154,6 @@ def compute_index_rows(
     if extensions_directory is not None:
         con.execute(SET_EXTENSIONS_DIRECTORY_COMMAND.format(directory=extensions_directory))
 
-    con.execute(INSTALL_EXTENSION_COMMAND.format(extension="httpfs"))
-    con.execute(LOAD_EXTENSION_COMMAND.format(extension="httpfs"))
     con.execute(INSTALL_EXTENSION_COMMAND.format(extension="fts"))
     con.execute(LOAD_EXTENSION_COMMAND.format(extension="fts"))
 
