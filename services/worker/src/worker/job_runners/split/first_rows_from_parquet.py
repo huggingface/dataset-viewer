733dcea5f489a82a65ff5affeecad50d33324a1a
Andrea Francis Soria Jimenez <andrea@huggingface.co>
2023-11-17 10:37:29 -0400

    Split parquet schema mismatch (#2124)
    * catch exception
    
    * catch Arrow exception and move tests
    
    * add test
    
    * remove unnecesary code
    
    * clear message

--- services/worker/src/worker/job_runners/split/first_rows_from_parquet.py
+++ services/worker/src/worker/job_runners/split/first_rows_from_parquet.py
@@ -12,10 +12,11 @@ from libcommon.constants import (
 from libcommon.exceptions import (
     ParquetResponseEmptyError,
     RowsPostProcessingError,
+    SplitParquetSchemaMismatchError,
     TooBigContentError,
     TooManyColumnsError,
 )
-from libcommon.parquet_utils import EmptyParquetMetadataError, Indexer, TooBigRows
+from libcommon.parquet_utils import EmptyParquetMetadataError, Indexer, SchemaMismatchError, TooBigRows
 from libcommon.processing_graph import ProcessingGraph, ProcessingStep
 from libcommon.public_assets_storage import PublicAssetsStorage
 from libcommon.storage import StrPath
@@ -80,6 +81,11 @@ def compute_first_rows_response(
         )
     except EmptyParquetMetadataError:
         raise ParquetResponseEmptyError("No parquet files found.")
+    except SchemaMismatchError as err:
+        raise SplitParquetSchemaMismatchError(
+            "Split parquet files being processed have different schemas. Ensure all files have identical column names.",
+            cause=err,
+        )
 
     # validate the features
     features = rows_index.parquet_index.features
