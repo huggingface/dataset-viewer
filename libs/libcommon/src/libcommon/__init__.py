5b80304f8acfdaa17db677e48edf8381568f2765
Quentin Lhoest <lhoest.q@gmail.com>
2024-01-04 16:31:29 +0100

    add comment

--- libs/libcommon/src/libcommon/__init__.py
+++ libs/libcommon/src/libcommon/__init__.py
@@ -3,4 +3,6 @@
 
 from datasets import config as _datasets_config
 
+# This is just to make `datasets` faster:
+# no need to check for a Parquet export since we will build it
 _datasets_config.USE_PARQUET_EXPORT = False
