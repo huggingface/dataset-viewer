b340d1667856250813cf5a29a236fd7f64402741
Test User <sylvain.lesage@huggingface.co>
2024-01-19 13:04:10 +0000

    add doc

--- libs/libcommon/src/libcommon/storage_client.py
+++ libs/libcommon/src/libcommon/storage_client.py
@@ -18,10 +18,11 @@ class StorageClient:
     A resource that represents a connection to a storage client.
 
     Args:
-        protocol (:obj:`str`): The fsspec protocol (supported s3 or file)
+        protocol (:obj:`str`): The fsspec protocol (supported: "file" or "s3")
         storage_root (:obj:`str`): The storage root path
         base_url (:obj:`str`): The base url for the publicly distributed assets
         overwrite (:obj:`bool`, `optional`, defaults to :obj:`False`): Whether to overwrite existing files
+        s3_config (:obj:`S3Config`, `optional`): The S3 configuration to connect to the storage client. Only needed if the protocol is "s3"
         cloudfront_config (:obj:`CloudFrontConfig`, `optional`): The CloudFront configuration to generate signed urls
     """
 
