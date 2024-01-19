dd050fa21b4f65ad5d6e13dd4f0daf78cbff0bb0
Test User <sylvain.lesage@huggingface.co>
2024-01-19 13:59:54 +0000

    clearer tests

--- libs/libcommon/tests/test_storage_client.py
+++ libs/libcommon/tests/test_storage_client.py
@@ -22,32 +22,33 @@ class FakeUrlSigner(URLSigner):
 
 
 URL = f"{BASE_URL}/file.txt"
-STRING_WITH_URL_AT_THE_START = f"{URL} and some other text"
-OBJECT_WITH_URL = {"key": URL, "key2": URL}
-ARRAY_WITH_URL = [URL, URL]
-NESTED_OBJECT_WITH_URL = {"key": {"key2": URL}, "key2": URL, "key3": {"key4": [URL]}}
-
 STRING_WITH_NO_URL = "string with no url"
 STRING_WITH_URL_INSIDE = f"string with url inside {URL}"
 OBJECT_WITH_NO_URL = {"key": "value"}
 ARRAY_WITH_NO_URL = ["value1", "value2"]
+OBJECT_WITH_URL = {"key": URL, "key2": URL}
+ARRAY_WITH_URL = [URL, URL]
+NESTED_OBJECT_WITH_URL = {"key": {"key2": URL}, "key2": URL, "key3": {"key4": [URL]}}
+STRING_WITH_URL_AT_THE_START = f"{URL} and some other text"
+ALREADY_SIGNED_URL = f"{BASE_URL}/file.txt?already_signed=1"
 
 
 @pytest.mark.parametrize(
     "content,num_replaced_urls",
     [
-        (URL, 1),
-        (STRING_WITH_URL_AT_THE_START, 1),
-        (OBJECT_WITH_URL, 2),
-        (ARRAY_WITH_URL, 2),
-        (NESTED_OBJECT_WITH_URL, 3),
-        # ^ not expected, hopefully will not happen
-        # below: will not sign any URL
         (None, 0),
         (1, 0),
         (STRING_WITH_NO_URL, 0),
         (STRING_WITH_URL_INSIDE, 0),
         (OBJECT_WITH_NO_URL, 0),
+        (ARRAY_WITH_NO_URL, 0),
+        (URL, 1),
+        (OBJECT_WITH_URL, 2),
+        (ARRAY_WITH_URL, 2),
+        (NESTED_OBJECT_WITH_URL, 3),
+        # below are limitations, hopefully will not happen
+        (STRING_WITH_URL_AT_THE_START, 1),
+        (ALREADY_SIGNED_URL, 0),
     ],
 )
 def test_sign_urls_in_obj(content: Any, num_replaced_urls: int) -> None:
