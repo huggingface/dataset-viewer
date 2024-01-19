16479e7153aed0d48738850a920ce5f4f32aa5a2
Andrea Soria <andrea@huggingface.co>
2024-01-19 14:29:09 -0400

    try to error

--- services/search/src/search/duckdb_connection.py
+++ services/search/src/search/duckdb_connection.py
@@ -5,6 +5,9 @@ import duckdb
 
 def duckdb_connect(**kwargs: Any) -> duckdb.DuckDBPyConnection:
     con = duckdb.connect(read_only=True, **kwargs)
-    con.sql("SET enable_external_access=false;")
+    # TODO: Temporary commented because it raises
+    # duckdb.duckdb.Error: Extension Autoloading Error:
+    # An error occurred while trying to automatically install the required extension
+    # con.sql("SET enable_external_access=false;")
     con.sql("SET lock_configuration=true;")
     return con
