e67610ce955a09db1b03bb1a0257b284206fce6a
Quentin Lhoest <lhoest.q@gmail.com>
2024-02-05 18:06:33 +0100

    add to hub-cache

--- libs/libcommon/src/libcommon/processing_graph.py
+++ libs/libcommon/src/libcommon/processing_graph.py
@@ -638,7 +638,7 @@ specification: ProcessingGraphSpecification = {
     },
     "dataset-hub-cache": {
         "input_type": "dataset",
-        "triggered_by": ["dataset-is-valid", "dataset-size"],
+        "triggered_by": ["dataset-is-valid", "dataset-size", "dataset-loading-tags"],
         "job_runner_version": 1,
         "difficulty": 20,
     },
