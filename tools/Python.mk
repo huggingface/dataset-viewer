1d3fb73dc33e1cb25f6226d3ae7940496adf5b9d
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-02-06 16:56:39 +0100

    Use POETRY variable in Makefiles

--- tools/Python.mk
+++ tools/Python.mk
@@ -4,23 +4,23 @@ POETRY := $(if $(POETRY),$(POETRY),$(POETRY_DEFAULT))
 
 .PHONY: install
 install:
-	poetry install
+	$(POETRY) install
 
 .PHONY: lock
 lock:
 	rm -rf .venv/
 	rm -f poetry.lock
-	poetry lock
-	poetry install
+	$(POETRY) lock
+	$(POETRY) install
 
 # Check that source code meets quality standards + security
 .PHONY: quality
 quality:
-	poetry run ruff check src
-	poetry run ruff check tests --ignore=ARG
-	poetry run ruff format --check src tests
-	poetry run mypy tests src
-	poetry run bandit -r src
+	$(POETRY) run ruff check src
+	$(POETRY) run ruff check tests --ignore=ARG
+	$(POETRY) run ruff format --check src tests
+	$(POETRY) run mypy tests src
+	$(POETRY) run bandit -r src
 
 #$(MAKE) pip-audit
 # ^ 20231121 - disabled until we upgrade to huggingface-hub@0.20
@@ -28,11 +28,11 @@ quality:
 # Format source code automatically
 .PHONY: style
 style:
-	poetry run ruff check --fix src
-	poetry run ruff check --fix tests --ignore=ARG
-	poetry run ruff format src tests
+	$(POETRY) run ruff check --fix src
+	$(POETRY) run ruff check --fix tests --ignore=ARG
+	$(POETRY) run ruff format src tests
 
 .PHONY: pip-audit
 pip-audit:
-	bash -c "poetry run pip-audit --ignore-vuln GHSA-wfm5-v35h-vwf4 --ignore-vuln GHSA-cwvm-v4w8-q58c --ignore-vuln PYSEC-2022-43059 -r <(poetry export -f requirements.txt --with dev  | sed '/^kenlm @/d' |sed '/^torch @/d' | sed '/^libapi @/d' | sed '/^libcommon @/d' | sed '/^trec-car-tools @/d')"
+	bash -c "$(POETRY) run pip-audit --ignore-vuln GHSA-wfm5-v35h-vwf4 --ignore-vuln GHSA-cwvm-v4w8-q58c --ignore-vuln PYSEC-2022-43059 -r <(poetry export -f requirements.txt --with dev  | sed '/^kenlm @/d' |sed '/^torch @/d' | sed '/^libapi @/d' | sed '/^libcommon @/d' | sed '/^trec-car-tools @/d')"
 # ^ we remove problematic lines to have a working pip-audit. See https://github.com/pypa/pip-audit/issues/84#issuecomment-1326203111 for "requests"
