e72a5b80f027c857edca9a8723a0f218e51193e3
Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>
2024-02-06 16:50:52 +0100

    Add loading tags, starting with croissant and webdataset (#2386)
    * tags
    
    * fix tests
    
    * add tags to dataset-hub-cache
    
    * add to hub-cache
    
    * bump dataset-hub-cache version
    
    * style
    
    * add tests
    
    * add DatasetLoadingTag type
    
    * use CROISSANT_MAX_CONFIGS constant
    
    * don't increase hub-cachd version
    
    * add migration
    
    * fix test
    
    * Update jobs/mongodb_migration/src/mongodb_migration/migrations/_20240206153000_cache_add_tags_in_hub_cache.py
    
    Co-authored-by: Sylvain Lesage <sylvain.lesage@huggingface.co>
    
    ---------
    
    Co-authored-by: Sylvain Lesage <sylvain.lesage@huggingface.co>

--- services/worker/src/worker/job_runners/dataset/hub_cache.py
+++ services/worker/src/worker/job_runners/dataset/hub_cache.py
@@ -4,9 +4,9 @@
 import logging
 
 from libcommon.exceptions import PreviousStepFormatError
-from libcommon.simple_cache import get_previous_step_or_raise
+from libcommon.simple_cache import CachedArtifactNotFoundError, get_previous_step_or_raise
 
-from worker.dtos import DatasetHubCacheResponse, JobResult
+from worker.dtos import DatasetHubCacheResponse, DatasetLoadingTag, JobResult
 from worker.job_runners.dataset.dataset_job_runner import DatasetJobRunner
 
 
@@ -63,12 +63,24 @@ def compute_hub_cache_response(dataset: str) -> tuple[DatasetHubCacheResponse, f
 
     progress = min((p for p in [is_valid_progress, size_progress] if p is not None), default=0.0)
 
+    tags: list[DatasetLoadingTag] = []
+    try:
+        loading_tags_response = get_previous_step_or_raise(kinds=["dataset-loading-tags"], dataset=dataset)
+        tags = loading_tags_response.response["content"]["tags"]
+    except CachedArtifactNotFoundError:
+        logging.info(f"Missing 'dataset-loading-tags' response for {dataset=}")
+    except KeyError:
+        raise PreviousStepFormatError(
+            "Previous step 'dataset-loading-tags' did not return the expected content: 'tags''."
+        )
+
     return (
         DatasetHubCacheResponse(
             preview=preview,
             viewer=viewer,
             partial=partial,
             num_rows=num_rows,
+            tags=tags,
         ),
         progress,
     )
