95ba547e3c540ec246865c10593b0c7a660f5abf
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-02-19 16:39:32 +0100

    Delete coverage Sphinx extension from vendors

--- services/worker/vendors/trec-car-tools/python3/conf.py
+++ services/worker/vendors/trec-car-tools/python3/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.coverage',
+    # 'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.autodoc',
