c3f4ba2a3d28f08acd1fd44ca29b66476a6e8c5d
Sylvain Lesage <sylvain.lesage@huggingface.co>
2024-02-20 14:09:07 +0000

    remove dependency 'trec-car-tools' from worker

--- services/worker/vendors/trec-car-tools/python3/trec_car/__init__.py
+++
@@ -1,5 +0,0 @@
-"""__init__ module for trec-car-tools, imports all necessary functions for reading cbor data provided in the TREC CAR"""
-
-__version__ = 1.0
-
-__all__ = ['read_data', 'format_runs']
