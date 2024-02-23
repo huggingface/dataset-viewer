3215f9539878621455c8b369ee9738452b53a0cd
Sylvain Lesage <sylvain.lesage@huggingface.co>
2024-02-23 14:11:48 +0000

    refactor

--- jobs/cache_maintenance/src/cache_maintenance/cache_metrics.py
+++ jobs/cache_maintenance/src/cache_maintenance/cache_metrics.py
@@ -15,33 +15,25 @@ def collect_cache_metrics() -> None:
 
     The obsolete cache metrics are deleted, and the new ones are inserted or updated.
 
-    We don't delete everythig, then create everything, because the /metrics endpoint could be called at the same time,
+    We don't delete everything, then create everything, because the /metrics endpoint could be called at the same time,
     and the metrics would be inconsistent.
     """
     logging.info("updating cache metrics")
-    new_count_entries = get_responses_count_by_kind_status_and_error_code()
-    print(f"{new_count_entries=}")
-    new_total_by_id = {
-        (metric["kind"], metric["http_status"], metric["error_code"]): metric["count"] for metric in new_count_entries
-    }
-    print(f"{new_total_by_id=}")
-    new_ids = set(new_total_by_id.keys())
-    print(f"{new_ids=}")
+    new_metric_by_id = get_responses_count_by_kind_status_and_error_code()
+    new_ids = set(new_metric_by_id.keys())
     old_ids = set(
         (metric.kind, metric.http_status, metric.error_code) for metric in CacheTotalMetricDocument.objects()
     )
-    print(f"{old_ids=}")
     to_delete = old_ids - new_ids
-    print(f"{to_delete=}")
 
     for kind, http_status, error_code in to_delete:
         CacheTotalMetricDocument.objects(kind=kind, http_status=http_status, error_code=error_code).delete()
-        print(f"{kind=} {http_status=} {error_code=} has been deleted")
+        logging.info(f"{kind=} {http_status=} {error_code=} has been deleted")
 
-    for (kind, http_status, error_code), total in new_total_by_id.items():
+    for (kind, http_status, error_code), total in new_metric_by_id.items():
         CacheTotalMetricDocument.objects(kind=kind, http_status=http_status, error_code=error_code).upsert_one(
             total=total
         )
-        print(f"{kind=} {http_status=} {error_code=}: {total=} has been inserted")
+        logging.info(f"{kind=} {http_status=} {error_code=}: {total=} has been inserted")
 
-    logging.info("cache metrics have been updating")
+    logging.info("cache metrics have been updated")
--- jobs/cache_maintenance/tests/test_collect_cache_metrics.py
+++ jobs/cache_maintenance/tests/test_collect_cache_metrics.py
@@ -4,7 +4,7 @@
 from unittest.mock import patch
 
 import pytest
-from libcommon.simple_cache import CacheTotalMetricDocument, CountEntry
+from libcommon.simple_cache import CacheTotalMetricDocument, EntriesTotalByKindStatusAndErrorCode
 
 from cache_maintenance.cache_metrics import collect_cache_metrics
 
@@ -13,42 +13,38 @@ STATUS_500 = 500
 ERROR_A = "ErrorA"
 COUNT = 1
 
-COUNT_ENTRY = CountEntry(kind=KIND_A, http_status=STATUS_500, error_code=ERROR_A, count=COUNT)
-COUNT_ENTRY_KIND = CountEntry(kind="kindB", http_status=STATUS_500, error_code=ERROR_A, count=COUNT)
-COUNT_ENTRY_STATUS = CountEntry(kind=KIND_A, http_status=400, error_code=ERROR_A, count=COUNT)
-COUNT_ENTRY_ERROR = CountEntry(kind=KIND_A, http_status=STATUS_500, error_code="ErrorB", count=COUNT)
-COUNT_ENTRY_ERROR_NONE = CountEntry(kind=KIND_A, http_status=STATUS_500, error_code=None, count=COUNT)
-COUNT_ENTRY_COUNT = CountEntry(kind=KIND_A, http_status=STATUS_500, error_code=ERROR_A, count=COUNT + 1)
+NEW_METRIC = {(KIND_A, STATUS_500, ERROR_A): COUNT}
+OTHER_KIND = {("kindB", STATUS_500, ERROR_A): COUNT}
+OTHER_STATUS = {(KIND_A, 400, ERROR_A): COUNT}
+OTHER_ERROR = {(KIND_A, STATUS_500, "ErrorB"): COUNT}
+NONE_ERROR = {(KIND_A, STATUS_500, None): COUNT}
+OTHER_COUNT = {(KIND_A, STATUS_500, ERROR_A): COUNT + 1}
 
 
 @pytest.mark.parametrize(
-    "metrics,new_count_entries",
+    "old_metrics",
     [
-        ([], [COUNT_ENTRY]),
-        ([COUNT_ENTRY], [COUNT_ENTRY]),
-        ([COUNT_ENTRY_KIND], [COUNT_ENTRY]),
-        ([COUNT_ENTRY_STATUS], [COUNT_ENTRY]),
-        ([COUNT_ENTRY_ERROR], [COUNT_ENTRY]),
-        ([COUNT_ENTRY_ERROR_NONE], [COUNT_ENTRY]),
-        ([COUNT_ENTRY_COUNT], [COUNT_ENTRY]),
+        {},
+        NEW_METRIC,
+        OTHER_KIND,
+        OTHER_STATUS,
+        OTHER_ERROR,
+        NONE_ERROR,
+        OTHER_COUNT,
+        {**NEW_METRIC, **OTHER_KIND, **OTHER_STATUS, **OTHER_ERROR, **NONE_ERROR, **OTHER_COUNT},
     ],
 )
-def test_collect_cache_metrics(metrics: list[CountEntry], new_count_entries: list[CountEntry]) -> None:
-    for metric in metrics:
-        CacheTotalMetricDocument(
-            kind=metric["kind"],
-            http_status=metric["http_status"],
-            error_code=metric["error_code"],
-            total=metric["count"],
-        ).save()
+def test_collect_cache_metrics(old_metrics: EntriesTotalByKindStatusAndErrorCode) -> None:
+    for (kind, http_status, error_code), total in old_metrics.items():
+        CacheTotalMetricDocument(kind=kind, http_status=http_status, error_code=error_code, total=total).save()
 
     with patch(
         "cache_maintenance.cache_metrics.get_responses_count_by_kind_status_and_error_code",
-        return_value=new_count_entries,
+        return_value=NEW_METRIC,
     ):
         collect_cache_metrics()
 
-    assert [
-        CountEntry(kind=metric.kind, http_status=metric.http_status, error_code=metric.error_code, count=metric.total)
+    assert {
+        (metric.kind, metric.http_status, metric.error_code): metric.total
         for metric in CacheTotalMetricDocument.objects()
-    ] == new_count_entries
+    } == NEW_METRIC
