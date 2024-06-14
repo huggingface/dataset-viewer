b908f86e9737fa0b84466da61976a321c3187078
Sylvain Lesage <sylvain.lesage@huggingface.co>
2024-06-14 13:00:01 +0000

    refresh all the entries

--- libs/libcommon/src/libcommon/processing_graph.py
+++ libs/libcommon/src/libcommon/processing_graph.py
@@ -709,7 +709,7 @@ specification: ProcessingGraphSpecification = {
     "dataset-modalities": {
         "input_type": "dataset",
         "triggered_by": ["dataset-info", "dataset-filetypes"],
-        "job_runner_version": 1,
+        "job_runner_version": 2,
         "difficulty": 20,
     },
     "dataset-croissant-crumbs": {
