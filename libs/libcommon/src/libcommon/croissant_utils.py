a438bd45547f80b25ce92cc5e648f2b236a0889c
Pierre Marcenac <pierremarcenac@google.com>
2024-07-22 13:00:37 +0200

    Support `Sequence()` features in Croissant crumbs. (#2943)
    * Support `Sequence()` features in Croissant crumbs.
    
    * Fix style in services/worker/.
    
    * Apply ruff format.
    
    * make styke
    
    * Add type annotation for mypy.
    
    * Add more types for mypy.
    
    ---------
    
    Co-authored-by: Quentin Lhoest <42851186+lhoestq@users.noreply.github.com>

--- libs/libcommon/src/libcommon/croissant_utils.py
+++ libs/libcommon/src/libcommon/croissant_utils.py
@@ -2,7 +2,9 @@
 # Copyright 2024 The HuggingFace Authors.
 
 from collections.abc import Mapping
-from typing import Any
+from typing import Any, Union
+
+from datasets import ClassLabel, Image, Sequence, Value
 
 
 def get_record_set(dataset: str, config_name: str) -> str:
@@ -34,3 +36,68 @@ def truncate_features_from_croissant_crumbs_response(content: Mapping[str, Any])
                 record["description"] += (
                     f"\n- {num_columns - MAX_COLUMNS} skipped column{'s' if num_columns - MAX_COLUMNS > 1 else ''} (max number of columns reached)"
                 )
+
+
+HF_TO_CROISSANT_VALUE_TYPE = {
+    "binary": "sc:Text",
+    "bool": "sc:Boolean",
+    "float8": "sc:Float",
+    "float16": "sc:Float",
+    "float32": "sc:Float",
+    "float64": "sc:Float",
+    "int16": "sc:Integer",
+    "int32": "sc:Integer",
+    "int64": "sc:Integer",
+    "large_string": "sc:Text",
+    "string": "sc:Text",
+}
+
+
+def feature_to_croissant_field(
+    distribution_name: str, field_name: str, column: str, feature: Any
+) -> Union[dict[str, Any], None]:
+    """Converts a Hugging Face Datasets feature to a Croissant field or None if impossible."""
+    if isinstance(feature, Value) and feature.dtype in HF_TO_CROISSANT_VALUE_TYPE:
+        return {
+            "@type": "cr:Field",
+            "@id": field_name,
+            "name": field_name,
+            "description": f"Column '{column}' from the Hugging Face parquet file.",
+            "dataType": HF_TO_CROISSANT_VALUE_TYPE[feature.dtype],
+            "source": {"fileSet": {"@id": distribution_name}, "extract": {"column": column}},
+        }
+    elif isinstance(feature, Image):
+        return {
+            "@type": "cr:Field",
+            "@id": field_name,
+            "name": field_name,
+            "description": f"Image column '{column}' from the Hugging Face parquet file.",
+            "dataType": "sc:ImageObject",
+            "source": {
+                "fileSet": {"@id": distribution_name},
+                "extract": {"column": column},
+                "transform": {"jsonPath": "bytes"},
+            },
+        }
+    elif isinstance(feature, ClassLabel):
+        return {
+            "@type": "cr:Field",
+            "@id": field_name,
+            "name": field_name,
+            "description": f"ClassLabel column '{column}' from the Hugging Face parquet file.\nLabels:\n"
+            + ", ".join(f"{name} ({i})" for i, name in enumerate(feature.names)),
+            "dataType": "sc:Integer",
+            "source": {"fileSet": {"@id": distribution_name}, "extract": {"column": column}},
+        }
+    elif isinstance(feature, (Sequence, list)):
+        if isinstance(feature, Sequence):
+            sub_feature = feature.feature
+        else:
+            if len(feature) != 1:
+                return None
+            sub_feature = feature[0]
+        field = feature_to_croissant_field(distribution_name, field_name, column, sub_feature)
+        if field:
+            field["repeated"] = True
+            return field
+    return None
--- libs/libcommon/tests/test_croissant_utils.py
+++ libs/libcommon/tests/test_croissant_utils.py
@@ -2,11 +2,13 @@
 # Copyright 2024 The HuggingFace Authors.
 
 
+from typing import Any
 from unittest.mock import patch
 
 import pytest
+from datasets import Sequence, Value
 
-from libcommon.croissant_utils import truncate_features_from_croissant_crumbs_response
+from libcommon.croissant_utils import feature_to_croissant_field, truncate_features_from_croissant_crumbs_response
 
 
 @pytest.mark.parametrize("num_columns", [1, 3])
@@ -26,3 +28,50 @@ def test_truncate_features_from_croissant_crumbs_response(num_columns: int) -> N
     else:
         assert len(content["recordSet"][0]["field"]) == 2
         assert "max number of columns reached" in content["recordSet"][0]["description"]
+
+
+@pytest.mark.parametrize(
+    ("hf_datasets_feature", "croissant_field"),
+    [
+        (
+            Value(dtype="int32"),
+            {
+                "@type": "cr:Field",
+                "@id": "field_name",
+                "name": "field_name",
+                "description": "Column 'column_name' from the Hugging Face parquet file.",
+                "dataType": "sc:Integer",
+                "source": {"fileSet": {"@id": "distribution_name"}, "extract": {"column": "column_name"}},
+            },
+        ),
+        (
+            Sequence(Value(dtype="int32")),
+            {
+                "@type": "cr:Field",
+                "@id": "field_name",
+                "name": "field_name",
+                "description": "Column 'column_name' from the Hugging Face parquet file.",
+                "dataType": "sc:Integer",
+                "source": {"fileSet": {"@id": "distribution_name"}, "extract": {"column": "column_name"}},
+                "repeated": True,
+            },
+        ),
+        (
+            [Value(dtype="int32")],
+            {
+                "@type": "cr:Field",
+                "@id": "field_name",
+                "name": "field_name",
+                "description": "Column 'column_name' from the Hugging Face parquet file.",
+                "dataType": "sc:Integer",
+                "source": {"fileSet": {"@id": "distribution_name"}, "extract": {"column": "column_name"}},
+                "repeated": True,
+            },
+        ),
+    ],
+)
+def test_feature_to_croissant_field(hf_datasets_feature: Any, croissant_field: Any) -> None:
+    assert (
+        feature_to_croissant_field("distribution_name", "field_name", "column_name", hf_datasets_feature)
+        == croissant_field
+    )
