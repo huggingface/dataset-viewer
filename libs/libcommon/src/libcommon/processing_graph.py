10956bf5d1c1ce187b977854c1378934416f05c2
Quentin Lhoest <lhoest.q@gmail.com>
2024-07-24 18:50:43 +0200

    bump croissant job version

--- libs/libcommon/src/libcommon/processing_graph.py
+++ libs/libcommon/src/libcommon/processing_graph.py
@@ -715,7 +715,7 @@ specification: ProcessingGraphSpecification = {
     "dataset-croissant-crumbs": {
         "input_type": "dataset",
         "triggered_by": "dataset-info",
-        "job_runner_version": 1,
+        "job_runner_version": 2,
         "difficulty": 20,
     },
     "dataset-filetypes": {
