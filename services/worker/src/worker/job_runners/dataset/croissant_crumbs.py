00e1df82d322716d8eff7ef9d0b12bd209044047
ccl-core <91942859+ccl-core@users.noreply.github.com>
2024-10-01 13:53:05 +0200

    Update croissant_crumbs.py

--- services/worker/src/worker/job_runners/dataset/croissant_crumbs.py
+++ services/worker/src/worker/job_runners/dataset/croissant_crumbs.py
@@ -109,7 +109,7 @@ def get_croissant_crumbs_from_dataset_infos(
                 {
                     "@type": "cr:RecordSet",
                     "dataType": "cr:Split",
-                    "key": [{"@id": f"{split_record_set_name}/split_name"}],
+                    "key": {"@id": f"{split_record_set_name}/split_name"},
                     "@id": split_record_set_name,
                     "name": split_record_set_name,
                     "description": f"Splits for the {record_set_name} config.",
