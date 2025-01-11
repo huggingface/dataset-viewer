81e87218d7837d6c53414dece9a7cfa78356dad4
Parag Ekbote <thecoolekbote189@gmail.com>
2025-01-11 17:48:17 +0000

    Add environment variables in makefiles.

--- tools/Docker.mk
+++ tools/Docker.mk
@@ -1,3 +1,6 @@
+export API_HF_JWT_PUBLIC_KEY_URL := https://hub-ci.huggingface.co/api/keys/jwt
+export API_HF_JWT_ADDITIONAL_PUBLIC_KEYS :=
+
 .PHONY: down
 down:	
 	docker compose -f $(DOCKER_COMPOSE) down --remove-orphans --volumes
