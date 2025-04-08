3ecec857b1c9caa82cba2bf871123831745aa01e
Quentin Lhoest <lhoest.q@gmail.com>
2025-04-08 18:43:21 +0200

    fix full_text_search

--- services/search/src/search/duckdb_connection.py
+++ services/search/src/search/duckdb_connection.py
@@ -2,12 +2,15 @@ from typing import Any, Optional
 
 import duckdb
 
+ATTACH_READ_ONLY_DATABASE = "ATTACH '{database}' as db (READ_ONLY); USE db;"
 LOAD_FTS_SAFE_COMMAND = "INSTALL 'fts'; LOAD 'fts'; SET enable_external_access=false; SET lock_configuration=true;"
 SET_EXTENSIONS_DIRECTORY_COMMAND = "SET extension_directory='{directory}';"
 
 
-def duckdb_connect(extensions_directory: Optional[str] = None, **kwargs: Any) -> duckdb.DuckDBPyConnection:
-    con = duckdb.connect(read_only=True, **kwargs)
+def duckdb_connect(database: Optional[str] = None, extensions_directory: Optional[str] = None, **kwargs: Any) -> duckdb.DuckDBPyConnection:
+    con = duckdb.connect(":memory:", **kwargs)
+    if database is not None:
+        con.execute(ATTACH_READ_ONLY_DATABASE.format(database=database))
     if extensions_directory is not None:
         con.execute(SET_EXTENSIONS_DIRECTORY_COMMAND.format(directory=extensions_directory))
     con.sql(LOAD_FTS_SAFE_COMMAND)
