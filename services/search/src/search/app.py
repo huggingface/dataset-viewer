730043773d4a3a9ece90b140f3a98590b8499d77
Quentin Lhoest <lhoest.q@gmail.com>
2025-04-14 15:13:41 +0200

    enable for /search too

--- services/search/src/search/app.py
+++ services/search/src/search/app.py
@@ -99,6 +99,7 @@ def create_app_with_config(app_config: AppConfig) -> Starlette:
             endpoint=create_search_endpoint(
                 duckdb_index_file_directory=duckdb_index_cache_directory,
                 cached_assets_storage_client=cached_assets_storage_client,
+                parquet_metadata_directory=parquet_metadata_directory,
                 target_revision=app_config.duckdb_index.target_revision,
                 hf_endpoint=app_config.common.hf_endpoint,
                 hf_token=app_config.common.hf_token,
