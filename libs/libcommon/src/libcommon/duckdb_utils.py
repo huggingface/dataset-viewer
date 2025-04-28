9a927dd0b0c4f0388e2611cdf182d7863f251b99
Quentin Lhoest <lhoest.q@gmail.com>
2025-04-28 16:49:40 +0200

    Revert "debugging"
    This reverts commit d7305a8da36d2caf45c95b140632dab42aa95892.

--- libs/libcommon/src/libcommon/duckdb_utils.py
+++ libs/libcommon/src/libcommon/duckdb_utils.py
@@ -23,7 +23,16 @@ from libcommon.statistics_utils import (
     StringColumn,
 )
 
-DISABLED_DUCKDB_REF_BRANCH_DATASET_NAME_PATTERNS = ["*"]  # TODO: revert, this is for debugging
+DISABLED_DUCKDB_REF_BRANCH_DATASET_NAME_PATTERNS = [
+    "vevotx/*",
+    "openai/*",
+    "EleutherAI/*",
+    "HuggingFaceFW/*",
+    "TIGER-Lab/*",
+    "Rapidata/*",  # images
+    "MrDragonFox/*",  # audios
+    "*NoDuckdbRef*",
+]
 
 DATASET_TYPE = "dataset"
 DEFAULT_STEMMER = "none"  # Exact word matches
