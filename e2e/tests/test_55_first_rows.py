04822d916dc873d7be7d606c72590bb6397afdd3
Andrea Soria <andrea@huggingface.co>
2025-05-26 16:03:50 -0400

    Add missing test for first-rows and filter

--- e2e/tests/test_55_first_rows.py
+++ e2e/tests/test_55_first_rows.py
@@ -48,3 +48,26 @@ def test_first_rows_audios_endpoint(normal_user_audios_public_dataset: str) -> N
     # ensure the URL is valid
     response = poll(url, url="")
     assert response.status_code == 200, response
+
+
+def test_first_rows_pdfs_endpoint(normal_user_pdfs_public_dataset: str) -> None:
+    dataset = normal_user_pdfs_public_dataset
+    config, split = get_default_config_split()
+    rows_response = poll_until_ready_and_assert(
+        relative_url=f"/first-rows?dataset={dataset}&config={config}&split={split}",
+        dataset=dataset,
+        should_retry_x_error_codes=["ResponseNotFound"],
+        # ^ I had 404 errors without it. It should return something else at one point.
+    )
+    content = rows_response.json()
+    # ensure the URL is signed
+    url = content["rows"][0]["row"]["pdf"][0]["src"]
+    assert isinstance(url, str), url
+    assert "pdf.pdf?Expires=" in url, url
+    assert "&Signature=" in url, url
+    assert "&Key-Pair-Id=" in url, url
+    # ensure the URL has been signed only once
+    assert url.count("Expires") == 1, url
+    # ensure the URL is valid
+    response = poll(url, url="")
+    assert response.status_code == 200, response
