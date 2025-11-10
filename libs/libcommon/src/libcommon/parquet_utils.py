36543694f979ee1465a2a98721ab0a02a62f229a
Krisztian Szucs <szucs.krisztian@gmail.com>
2025-11-10 22:00:36 +0100

    chore: show revision

--- libs/libcommon/src/libcommon/parquet_utils.py
+++ libs/libcommon/src/libcommon/parquet_utils.py
@@ -524,7 +524,7 @@ class RowsIndex:
                 files = [str(f) for f in Path(self.parquet_metadata_directory).rglob("*")]
                 raise ValueError(
                     f"Error while querying libviewer.Dataset: {e} for dataset={self.dataset},"
-                    f" config={self.config}, split={self.split}. "
+                    f" config={self.config}, split={self.split}. Revision: {self.revision}. "
                     f"Parquet files: {self.parquet_files}. "
                     f"Parquet index files: {self.viewer_index.files}. "
                     f"Parquet metadata files: {files} at {self.parquet_metadata_directory}"
