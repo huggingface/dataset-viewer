8825f3920184f77ccb3b7cdf8b76b6b4c71b6a64
Krisztian Szucs <szucs.krisztian@gmail.com>
2025-11-25 23:56:21 +0100

    test(worker): fix first_rows test

--- services/worker/tests/job_runners/split/test_first_rows.py
+++ services/worker/tests/job_runners/split/test_first_rows.py
@@ -5,7 +5,7 @@ from collections.abc import Callable, Generator
 from dataclasses import replace
 from http import HTTPStatus
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
 from unittest.mock import patch
 
 import pytest
@@ -19,6 +19,7 @@ from libcommon.simple_cache import upsert_response
 from libcommon.storage import StrPath
 from libcommon.storage_client import StorageClient
 from libcommon.utils import get_json_size
+from libviewer import Dataset as OriginalDataset  # type: ignore
 
 from worker.config import AppConfig
 from worker.job_runners.split.first_rows import SplitFirstRowsJobRunner
@@ -158,59 +159,64 @@ def test_compute_from_parquet_libviewer(
         http_status=HTTPStatus.OK,
     )
 
+    def LocalDataset(*args: Any, data_store: Optional[str] = None, **kwargs: Any) -> OriginalDataset:
+        data_store = f"file://{ds_fs.local_root_dir}"
+        return OriginalDataset(*args, data_store=data_store, **kwargs)
+
     # copy the parquet file over to the parquet metadata directory where Dataset looks for the metadata file
     parquet_metadata_path = Path(parquet_metadata_directory) / fake_metadata_subpath
     ds_fs.get(f"{config}/{split}/0000.parquet", str(parquet_metadata_path))
 
-    job_runner = get_job_runner(
-        dataset,
-        config,
-        split,
-        replace(
-            app_config,
-            common=replace(app_config.common, hf_token=None),
-            first_rows=replace(
-                app_config.first_rows,
-                min_number=10,
-                max_bytes=rows_max_bytes,
-                min_cell_bytes=10,
-                columns_max_number=columns_max_number,
+    with patch("libviewer.Dataset", new=LocalDataset):
+        job_runner = get_job_runner(
+            dataset,
+            config,
+            split,
+            replace(
+                app_config,
+                common=replace(app_config.common, hf_token=None),
+                first_rows=replace(
+                    app_config.first_rows,
+                    min_number=10,
+                    max_bytes=rows_max_bytes,
+                    min_cell_bytes=10,
+                    columns_max_number=columns_max_number,
+                ),
             ),
-        ),
-    )
+        )
 
-    job_runner.pre_compute()
-    if error_code:
-        with pytest.raises(CustomError) as error_info:
-            job_runner.compute()
-        assert error_info.value.code == error_code
-    else:
-        response = job_runner.compute().content
-        assert get_json_size(response) <= rows_max_bytes
-        assert response
-        assert response["rows"]
-        assert response["features"]
-        assert len(response["rows"]) == 3  # testing file has 3 rows see config/train/0000.parquet file
-        assert len(response["features"]) == 2  # testing file has 2 columns see config/train/0000.parquet file
-        assert response["features"][0]["feature_idx"] == 0
-        assert response["features"][0]["name"] == "col1"
-        assert response["features"][0]["type"]["_type"] == "Value"
-        assert response["features"][0]["type"]["dtype"] == "int64"
-        assert response["features"][1]["feature_idx"] == 1
-        assert response["features"][1]["name"] == "col2"
-        assert response["features"][1]["type"]["_type"] == "Value"
-        assert response["features"][1]["type"]["dtype"] == "string"
-        assert response["rows"][0]["row_idx"] == 0
-        assert response["rows"][0]["truncated_cells"] == []
-        assert response["rows"][0]["row"] == {"col1": 1, "col2": "a"}
-        assert response["rows"][1]["row_idx"] == 1
-        assert response["rows"][1]["truncated_cells"] == []
-        assert response["rows"][1]["row"] == {"col1": 2, "col2": "b"}
-        assert response["rows"][2]["row_idx"] == 2
-        assert response["rows"][2]["truncated_cells"] == []
-        assert response["rows"][2]["row"] == {"col1": 3, "col2": "c"}
+        job_runner.pre_compute()
+        if error_code:
+            with pytest.raises(CustomError) as error_info:
+                job_runner.compute()
+            assert error_info.value.code == error_code
+        else:
+            response = job_runner.compute().content
+            assert get_json_size(response) <= rows_max_bytes
+            assert response
+            assert response["rows"]
+            assert response["features"]
+            assert len(response["rows"]) == 3  # testing file has 3 rows see config/train/0000.parquet file
+            assert len(response["features"]) == 2  # testing file has 2 columns see config/train/0000.parquet file
+            assert response["features"][0]["feature_idx"] == 0
+            assert response["features"][0]["name"] == "col1"
+            assert response["features"][0]["type"]["_type"] == "Value"
+            assert response["features"][0]["type"]["dtype"] == "int64"
+            assert response["features"][1]["feature_idx"] == 1
+            assert response["features"][1]["name"] == "col2"
+            assert response["features"][1]["type"]["_type"] == "Value"
+            assert response["features"][1]["type"]["dtype"] == "string"
+            assert response["rows"][0]["row_idx"] == 0
+            assert response["rows"][0]["truncated_cells"] == []
+            assert response["rows"][0]["row"] == {"col1": 1, "col2": "a"}
+            assert response["rows"][1]["row_idx"] == 1
+            assert response["rows"][1]["truncated_cells"] == []
+            assert response["rows"][1]["row"] == {"col1": 2, "col2": "b"}
+            assert response["rows"][2]["row_idx"] == 2
+            assert response["rows"][2]["truncated_cells"] == []
+            assert response["rows"][2]["row"] == {"col1": 3, "col2": "c"}
 
-    job_runner.post_compute()
+        job_runner.post_compute()
 
 
 @pytest.mark.parametrize(
