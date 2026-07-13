8de854ca1bc27a534072901f3f8d9662ee8a5c21
Quentin Lhoest <lhoest.q@gmail.com>
2026-07-13 16:07:42 +0200

    and file

--- libs/libcommon/src/libcommon/fsspec.py
+++ libs/libcommon/src/libcommon/fsspec.py
@@ -1,5 +1,5 @@
 from fsspec.registry import known_implementations
 
 for name in list(known_implementations):
-    if name not in ["hf", "s3", "zip"]:
+    if name not in ["hf", "s3", "zip", "file"]:
         del known_implementations[name]
